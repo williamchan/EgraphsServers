86ddfca2d0fc3d5fb60f293a362a765d1cf636e1
Erem Boto <ehboto@gmail.com>
2011-10-20 15:59:26 -0700

    Fixes pluralization of Celebrities CRUD controller.

---
+++ app/controllers/Celebrities.scala
@@ -0,0 +1,6 @@
+package controllers
+
+import models.Celebrity
+
+@CRUD.For(classOf[Celebrity])
+class Celebrities extends CRUD
--- app/controllers/Celebritys.scala
+++
@@ -1,6 +0,0 @@
-package controllers
-
-import models.Celebrity
-
-//@CRUD.For(Celebrity.me.getClass.asInstanceOf[play.db.Model])
-class Celebritys extends CRUD
