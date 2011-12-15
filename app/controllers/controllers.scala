bef2db4930884a89a318818418d81ff9a54b6137
William Chan <will@egraphs.com>
2011-12-14 21:36:39 -0800

    Moving createAlphaTesters() out of Bootstrap and into a test controller. Now, test/create-alpha-testers is the route to hit to create alpha testers' accounts.

--- app/controllers/controllers.scala
+++ app/controllers/controllers.scala
@@ -1,6 +1,10 @@
 package controllers
 
 import play.mvc._
+import java.io.File
+import libs.Blobs.Conversions._
+import math.BigDecimal._
+import models.{Account, Celebrity}
 
 object Application extends Controller {
 
@@ -11,3 +15,55 @@ object Application extends Controller {
   }
 }
 
+object test extends Controller
+with DBTransaction {
+
+  import org.squeryl.PrimitiveTypeMode._
+
+  def createAlphaTesters(): String = {
+//    inTransaction {
+      createCelebrity("Erem", "Boto", "erem@egraphs.com")
+      createCelebrity("Andrew", "Smith", "andrew@egraphs.com")
+      createCelebrity("David", "Auld", "david@egraphs.com")
+      createCelebrity("Eric", "Feeny", "eric@egraphs.com")
+      createCelebrity("Will", "Chan", "will@egraphs.com")
+      createCelebrity("Zach", "Apter", "zachapter@gmail.com")
+      createCelebrity("Brian", "Auld", "bauld@raysbaseball.com")
+      createCelebrity("Michael", "Kalt", "mkalt@raysbaseball.com")
+      createCelebrity("Matt", "Silverman", "msilverman@raysbaseball.com")
+      createCelebrity("Gabe", "Kapler", "gabe@egraphs.com")
+//    }
+
+    "Alpha Testers created!"
+  }
+
+  private def createCelebrity(firstName: String, lastName: String, email: String) {
+    println("Creating Celebrity " + email + " ...")
+
+    val celebrity = Celebrity(
+      firstName = Some(firstName),
+      lastName = Some(lastName),
+      publicName = Some("Alpha " + firstName),
+      description = Some("Today's Sriracha is tomorrow's salsa")
+    ).save()
+
+    Account(email = email,
+      celebrityId = Some(celebrity.id)
+    ).withPassword("herp").right.get.save()
+    celebrity.saveWithProfilePhoto(new File("./test/files/E.jpg"))
+
+    celebrity.newProduct.copy(
+      priceInCurrency = 50,
+      name = firstName + "'s Alpha Product A",
+      description = "Tyson 15"
+    ).save()
+
+    celebrity.newProduct.copy(
+      priceInCurrency = 100,
+      name = firstName + "'s Alpha Product B",
+      description = "Help me... help YOU..."
+    ).save()
+
+  }
+
+}
