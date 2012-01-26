5b387699b87aa352d1a2693c6022957a0f3d518d
William Chan <will@egraphs.com>
2012-01-26 00:08:03 -0800

    Resolves #49: Draw signature strokes shorter than 4 points. Strokes with 1 point are just points, strokes with 2 points are drawn as lines, and strokes with 3 points are drawn as parabolas. Unit tested with single-point-stroke.jpg, two-point-stroke.jpg, and three-point-stroke.jpg.

--- app/libs/ImageUtil.scala
+++ app/libs/ImageUtil.scala
@@ -6,8 +6,8 @@ import java.awt.geom.Ellipse2D
 import javax.imageio.ImageIO
 import models.ImageAsset
 
-import java.io.{ByteArrayInputStream, ByteArrayOutputStream, File}
-import java.awt.{Transparency, Graphics, RenderingHints, Graphics2D, Color}
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+import java.awt.{Transparency, Graphics, RenderingHints, Graphics2D}
 
 object ImageUtil {
 
@@ -42,8 +42,8 @@ object ImageUtil {
     // Draw the shadow
     g.setColor(java.awt.Color.black)
     for (i <- 0 until originalXsByStroke.size) {
-      val xs = originalXsByStroke(i).map( penX => penX + 2 ).toArray
-      val ys = originalYsByStroke(i).map( penY => penY - 3 ).toArray
+      val xs = originalXsByStroke(i).map(penX => penX + 2).toArray
+      val ys = originalYsByStroke(i).map(penY => penY - 3).toArray
       drawStroke(g, xs, ys)
     }
 
@@ -81,15 +81,80 @@ object ImageUtil {
     require(xs.size == ys.size, "Xs and Ys must be of same length")
     val n = xs.size
 
-    // TODO(wchan): handle these cases
-    if (n < 4) return
-
     // Scale xs and ys
     for (i <- 0 until xs.length) {
       xs(i) = xs(i) * scaleFactor
       ys(i) = ys(i) * scaleFactor
     }
 
+    n match {
+      case 0 => return
+      case 1 => drawStrokeWith1Point(g, xs, ys)
+      case 2 => drawStrokeWith2Points(g, xs, ys)
+      case 3 => drawStrokeWith3Points(g, xs, ys)
+      case _ => drawStrokeWithManyPoints(g, xs, ys, n)
+    }
+  }
+
+  private def drawStrokeWith1Point(g: Graphics2D, xs: Array[Double], ys: Array[Double]) {
+    val drawX = xs.head
+    val drawY = ys.head
+    drawPoint(g, drawX, invertY(drawY))
+  }
+
+  /**
+   * Given 2 points, draw the points on the line between them. This uses the line equation.
+   */
+  private def drawStrokeWith2Points(g: Graphics2D, xs: Array[Double], ys: Array[Double]) {
+    val x1 = xs(0)
+    val x2 = xs(1)
+    val y1 = ys(0)
+    val y2 = ys(1)
+    val slope: Double = (y2 - y1) / (x2 - x1)
+    val x_increment: Double = (x2 - x1) / numIncrements
+
+    for (j <- 0 to numIncrements) {
+      val drawX = x1 + x_increment * j
+      val drawY = y1 + slope * drawX
+      drawPoint(g, drawX, invertY(drawY))
+    }
+  }
+
+  /**
+   * Given 3 points, first fit a parabolic curve using linear algebra to calculate a, b, and c in the equation:
+   * a*x*x + b*x + c = y
+   * The formulas for a, b, and c are discussed on:
+   * http://stackoverflow.com/questions/717762/how-to-calculate-the-vertex-of-a-parabola-given-three-points
+   */
+  private def drawStrokeWith3Points(g: Graphics2D, xs: Array[Double], ys: Array[Double]) {
+    val x1 = xs(0)
+    val x2 = xs(1)
+    val x3 = xs(2)
+    val y1 = ys(0)
+    val y2 = ys(1)
+    val y3 = ys(2)
+
+    val x1_to_x2_increment: Double = (x2 - x1) / numIncrements
+    val x2_to_x3_increment: Double = (x3 - x2) / numIncrements
+    val denominator: Double = (x1 - x2) * (x1 - x3) * (x2 - x3)
+    val a: Double = ((x3 * (y2 - y1)) + (x2 * (y1 - y3)) + (x1 * (y3 - y2))) / denominator
+    val b: Double = ((x3 * x3 * (y1 - y2)) + (x2 * x2 * (y3 - y1)) + (x1 * x1 * (y2 - y3))) / denominator
+    val c: Double = ((x2 * x3 * (x2 - x3) * y1) + (x3 * x1 * (x3 - x1) * y2) + (x1 * x2 * (x1 - x2) * y3)) / denominator
+
+    for (j <- 0 until numIncrements) {
+      val drawX = x1 + x1_to_x2_increment * j
+      val drawY = (a * drawX * drawX) + (b * drawX) + c
+      drawPoint(g, drawX, invertY(drawY))
+    }
+
+    for (j <- 1 to numIncrements) {
+      val drawX = x2 + x2_to_x3_increment * j
+      val drawY = (a * drawX * drawX) + (b * drawX) + c
+      drawPoint(g, drawX, invertY(drawY))
+    }
+  }
+
+  private def drawStrokeWithManyPoints(g: Graphics2D, xs: Array[Double], ys: Array[Double], n: Int) {
     // Initialize smoothedXs and smoothedYs
     val smoothedXs = xs.clone()
     val smoothedYs = ys.clone()
@@ -127,8 +192,7 @@ object ImageUtil {
     for (j <- 0 until numIncrements) {
       val drawX = bezier.calcXCoord(tNearC0 * j / numIncrements)
       val drawY = bezier.calcYCoord(tNearC0 * j / numIncrements)
-      // issue#7: calcPointSize based on ts
-      drawPoint(g, drawX, (height * scaleFactor).intValue() - drawY)
+      drawPoint(g, drawX, invertY(drawY))
     }
   }
 
@@ -136,15 +200,19 @@ object ImageUtil {
     for (j <- 0 until numIncrements) {
       val drawX = bezier.calcXCoord(tNearC0 + (1d - tNearC0) * j / (2 * numIncrements))
       val drawY = bezier.calcYCoord(tNearC0 + (1d - tNearC0) * j / (2 * numIncrements))
-      // issue#7: calcPointSize based on ts
-      drawPoint(g, drawX, (height * scaleFactor).intValue() - drawY)
+      drawPoint(g, drawX, invertY(drawY))
     }
   }
 
+  // issue#7: calcPointSize based on ts and dynamically determine pointSize
   private def drawPoint(g: Graphics2D, x: Double, y: Double, pointSize: Int = 4) {
     //    g.drawImage(penTipImage, x.intValue(), y.intValue(), null)
     val t = (pointSize * scaleFactor).intValue()
     g.draw(new Ellipse2D.Double(x, y, t, t))
+  }
+
+  private def invertY(y: Double): Double = {
+    (height * scaleFactor).intValue() - y
   }
 
   /**
@@ -160,75 +228,76 @@ object ImageUtil {
    * @param targetHeight the desired height of the scaled instance,
    *    in pixels
    * @param hint one of the rendering hints that corresponds to
-   *    {@code RenderingHints.KEY_INTERPOLATION} (e.g.
-   *    {@code RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR},
-   *    {@code RenderingHints.VALUE_INTERPOLATION_BILINEAR},
-   *    {@code RenderingHints.VALUE_INTERPOLATION_BICUBIC})
+   * { @code RenderingHints.KEY_INTERPOLATION} (e.g.
+   * { @code RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR},
+   * { @code RenderingHints.VALUE_INTERPOLATION_BILINEAR},
+   * { @code RenderingHints.VALUE_INTERPOLATION_BICUBIC})
    * @param higherQuality if true, this method will use a multi-step
    *    scaling technique that provides higher quality than the usual
    *    one-step technique (only useful in downscaling cases, where
-   *    {@code targetWidth} or {@code targetHeight} is
+   * { @code targetWidth} or { @code targetHeight} is
    *    smaller than the original dimensions, and generally only when
-   *    the {@code BILINEAR} hint is specified)
-   * @return a scaled version of the original {@code BufferedImage}
+   *    the { @code BILINEAR} hint is specified)
+   * @return a scaled version of the original { @code BufferedImage}
    */
   def getScaledInstance(
-    img: BufferedImage,
-    targetWidth: Int,
-    targetHeight: Int,
-    hint: Object = RenderingHints.VALUE_INTERPOLATION_BILINEAR,
-    higherQuality: Boolean = true): BufferedImage =
-  {
+                         img: BufferedImage,
+                         targetWidth: Int,
+                         targetHeight: Int,
+                         hint: Object = RenderingHints.VALUE_INTERPOLATION_BILINEAR,
+                         higherQuality: Boolean = true): BufferedImage = {
     require(
       targetWidth <= img.getWidth && targetHeight <= img.getHeight,
       "This method should only be used for down-scaling an image"
     )
     var imgType = if (img.getTransparency == Transparency.OPAQUE)
-        BufferedImage.TYPE_INT_RGB else BufferedImage.TYPE_INT_ARGB
+      BufferedImage.TYPE_INT_RGB
+    else BufferedImage.TYPE_INT_ARGB
     var ret: BufferedImage = img
-    var w:Int = 0
-    var h:Int = 0
+    var w: Int = 0
+    var h: Int = 0
     if (higherQuality) {
-        // Use multi-step technique: start with original size, then
-        // scale down in multiple passes with drawImage()
-        // until the target size is reached
-        w = img.getWidth
-        h = img.getHeight
+      // Use multi-step technique: start with original size, then
+      // scale down in multiple passes with drawImage()
+      // until the target size is reached
+      w = img.getWidth
+      h = img.getHeight
     } else {
-        // Use one-step technique: scale directly from original
-        // size to target size with a single drawImage() call
-        w = targetWidth
-        h = targetHeight
+      // Use one-step technique: scale directly from original
+      // size to target size with a single drawImage() call
+      w = targetWidth
+      h = targetHeight
     }
 
     do {
-        if (higherQuality && w > targetWidth) {
-            w /= 2
-            if (w < targetWidth) {
-                w = targetWidth
-            }
+      if (higherQuality && w > targetWidth) {
+        w /= 2
+        if (w < targetWidth) {
+          w = targetWidth
         }
+      }
 
-        if (higherQuality && h > targetHeight) {
-            h /= 2
-            if (h < targetHeight) {
-                h = targetHeight
-            }
+      if (higherQuality && h > targetHeight) {
+        h /= 2
+        if (h < targetHeight) {
+          h = targetHeight
         }
+      }
 
-        var tmp = new BufferedImage(w, h, imgType)
-        var g2 = tmp.createGraphics()
-        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint)
-        g2.drawImage(ret, 0, 0, w, h, null)
-        g2.dispose()
+      val tmp = new BufferedImage(w, h, imgType)
+      val g2 = tmp.createGraphics()
+      g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint)
+      g2.drawImage(ret, 0, 0, w, h, null)
+      g2.dispose()
 
-        ret = tmp
+      ret = tmp
     } while (w != targetWidth || h != targetHeight)
 
     ret
   }
 
   object Conversions {
+
     class RichBufferedImage(img: BufferedImage) {
       def asByteArray(imageType: ImageAsset.ImageType) = {
         val bytesOut = new ByteArrayOutputStream()
@@ -251,4 +320,5 @@ object ImageUtil {
       new ImageEnrichedByteArray(bytes)
     }
   }
+
 }
