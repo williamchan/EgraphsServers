05ce9e757f5da3229b4d7f792f2bf8da571fd7fc
Erem Boto <ehboto@gmail.com>
2012-03-28 14:30:42 -0700

    Documentation and minor improvement to ControllerMethod

--- app/services/http/ControllerMethod.scala
+++ app/services/http/ControllerMethod.scala
@@ -10,12 +10,34 @@ import services.db.{TransactionIsolation, TransactionSerializable, DBSession}
  * should begin with a call to this class.
  */
 class ControllerMethod @Inject()(logging: LoggingContext, db: DBSession) {
-  def apply[A](dbIsolation: TransactionIsolation = TransactionSerializable)
+
+  /**
+   * Prepares and customizes controller method behavior. The first expression in
+   * any controller methods in our codebase should be to call this function.
+   *
+   * @param openDatabase true that the a database connection should be managed
+   *    by this ControllerMethod instance.
+   * @param dbIsolation the transaction isolation with which to connect to the
+   *    database if openDatabase is true
+   * @param operation
+   *    the code block to execute after setting up the connection resources
+   * @param request
+   *    the request being served
+   *
+   * @return the result of the `operation` code block.
+   */
+  def apply[A](openDatabase:Boolean=true,
+               dbIsolation: TransactionIsolation = TransactionSerializable)
               (operation: => A)
               (implicit request: Request): A =
   {
     logging.withContext(request) {
-      db.connected(dbIsolation) {
+      if (openDatabase) {
+        db.connected(dbIsolation) {
+          operation
+        }
+      }
+      else {
         operation
       }
     }
