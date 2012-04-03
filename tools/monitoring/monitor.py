5a4aa580d0ed39b503c1adcaad45c7da26840631
Erem Boto <ehboto@gmail.com>
2012-04-03 14:49:13 -0700

    Quick bugfix

--- tools/monitoring/monitor.py
+++ tools/monitoring/monitor.py
@@ -12,11 +12,11 @@ if len(sys.argv) < 2:
 else:
     log_file = sys.argv[1]
     service_name = os.path.basename(log_file)
-    monitor = log_monitor.LogMonitor(service_name=service_name)
+    monitor = log_monitor.LogMonitor(service_name=service_name, ignore_repeats=True)
     monitor.add_listener(log_monitor.ErrorLogListener())
     monitor.add_listener(log_monitor.InactivityLogListener(service_name=service_name))
 
-    t = filetail.Tail(log_file, only_new=True, ignore_repeats=True)
+    t = filetail.Tail(log_file, only_new=True)
 
     print "Beginning to monitor service \"%s\" (%s)" % (service_name, log_file)
     for line in t:
