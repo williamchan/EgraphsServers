ba2c9945b51aed5df5a0c6b454789c046996ae8e
Erem Boto <ehboto@gmail.com>
2012-05-23 07:16:28 -0700

    Adds some front-end examples of server-side template

---
+++ .modules/frontend/app/models/frontend/models.scala
@@ -0,0 +1,34 @@
+//
+// Models for use in the front-end project. Generally you shouldn't
+// have to use these and should prefer using primitives (e.g. String,
+// Int, Date, etc), but sometimes you have to manipulate lists of
+// related data (e.g. lists of Product, written as Seq[Product]), in
+// which case a small case class is a-OK.
+//
+// DO let your views drive the contents of these "models". Though that
+// may seem strange, When the views get integrated into the main
+// application then implicit conversions will be written to tern the
+// *actual* model classes to and from the types you specify here.
+//
+// More about case classes: http://www.codecommit.com/blog/scala/case-classes-are-cool
+//
+
+package models
+
+/**
+ * An example of what a Product might look like. It has been namespaced to Frontend
+ * to distinguish it from the canonical server-side Product.
+ *
+ * @param id the product's unique ID
+ * @param name the product's name
+ * @param price the product's price in dollars
+ * @param description a long-form description of the product.
+ * @param url link to the product in the Single Celebrity Storefront.
+ */
+case class ExampleFrontendProduct(
+  id: String, 
+  name: String, 
+  price: String, 
+  description: String = "", // You can optionally provide default values
+  url: String = ""
+)
--- .modules/frontend/test-app/app/controllers.scala
+++ .modules/frontend/test-app/app/controllers.scala
@@ -1,14 +1,45 @@
+//
+// Controllers for testing the views you write in .modules/frontend/app/views
+// Use alongside .modules/frontend/app/test-app/conf/routes to render your pages.
+//
 package controllers
 
 import play._
 import play.mvc._
+import models._ // This gives us ExampleFrontendProject
 
 object Application extends Controller {
   import views.frontend
   
   def index = {
-    frontend.html.test("Hello Derp")
-//    html.index("Your Scala application is ready!")
+    // Render the landing page
+    frontend.example.html.simple("herp derp")
   }
-  
+
+  /**
+   * Shows a very simple template. Located at /Application/simple_example
+   */
+  def simple_example = {
+    frontend.example.html.simple("herp derp")
+  }
+
+  /**
+   * Shows how to pass a list of model objects into a template. /Application/model_example
+   */
+  def model_example = {
+    val products = Seq(
+      ExampleFrontendProduct(id="1", name="My First Product", price="50"),
+      ExampleFrontendProduct(id="2", name="My Second Product", price="100")
+    )
+    
+    frontend.example.html.uses_model(products)
+  }
+
+  /**
+   * Shows how to re-use code by passing templates into other templates.
+   * In this case base_template_example.
+   */
+  def code_reuse_example = {
+    frontend.example.html.uses_base_template()
+  }
 }
