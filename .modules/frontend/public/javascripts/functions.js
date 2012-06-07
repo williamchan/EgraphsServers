8d1f211ed6bdbc376511c284f52fd821d2583720
Ryan Scherler <ryan@crashpaddesign.com>
2012-06-06 18:42:00 -0700

    Much work done on responsiveness & continued updates to main content. EFE-26

--- .modules/frontend/public/javascripts/functions.js
+++ .modules/frontend/public/javascripts/functions.js
@@ -23,5 +23,33 @@ $(document).ready(function(){
 	    $('html,body').animate({scrollTop: $($(this).attr('href')).offset().top},'slow');	    
 	    e.preventDefault();	    
 	});
+	
+	
+		var landing_celebrities_btn = $('#landing-stars h3 a');
+		var landing_celebrities = $('#landing-stars .celebrities');
+	
+		landing_celebrities.hide().css('opacity', '0');		
+		
+		landing_celebrities_btn.hover(function(){		
+			$(this).parent().animate({ top: '-80px' }, 200);	
+		}, function() {
+			$(this).parent().animate({ top: '-75px' }, 200);
+		});
+
+		if (screen.width > 480) {	
+				landing_celebrities_btn.toggle(function(e){				
+					landing_celebrities.slideDown('fast').animate({ opacity: 1 });												
+					e.preventDefault();	
+				}, function(e) {
+					landing_celebrities.animate({ opacity: 0 }).slideUp('fast');
+					e.preventDefault();	
+				});
+		} else {
+			landing_celebrities_btn.click(function(e){		
+				$('html,body').animate({scrollTop: $($(this).attr('href')).offset().top},'slow');	
+				e.preventDefault();	
+			});
+		}
+
 
 });
