f7db01ded61b6b12bc302c2180fe926edca3281c
Erem Boto <ehboto@gmail.com>
2012-06-08 16:00:47 -0700

    Fixes issues from code review. SER-47

--- db/evolutions/10.sql
+++ db/evolutions/10.sql
@@ -1,14 +1,18 @@
-# Adds resetPasswordKey and fbUserId to account
+# Adds isFeatured and roleDescription to the celebrity table
 # --- !Ups
 
-ALTER TABLE celebrity ADD COLUMN isFeatured boolean NOT NULL;
+ALTER TABLE celebrity ADD COLUMN isFeatured boolean;
+UPDATE celebrity SET isFeatured = false;
+ALTER TABLE celebrity ALTER COLUMN isFeatured SET NOT NULL;
+
 ALTER TABLE celebrity ADD COLUMN roleDescription varchar(128);
 
 CREATE index idx512107dd on celebrity (isFeatured);
 
 # --- !Downs
 
-ALTER TABLE celebrity DROP COLUMN isFeatured;
-ALTER TABLE celebrity DROP COLUMN roleDescription;
-
 DROP index idx512107dd;
+
+ALTER TABLE celebrity DROP COLUMN roleDescription;
+ALTER TABLE celebrity DROP COLUMN isFeatured;
+
