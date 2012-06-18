a31c8bcb6e24e9388111dfbfc044ee41a9286671
Siegfried Bilstein <sig@egraphs.com>
2012-06-18 10:56:45 -0700

    EFE-23 interactive form functionality close to complete, added password, address forms, refactored ng widgets
    TODO:
    need validation for URLs/usernames
    hammer on UI tweaks
    submit functionality
    expand target on edit rows
    six vertical spacing issue when clicking on target rows

---
+++ .modules/frontend/app/views/frontend/tags/ng/Utils.scala
@@ -0,0 +1,13 @@
+package views.frontend.tags.ng
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: siegfried
+ * Date: 6/15/12
+ * Time: 11:08 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Utils {
+
+}
