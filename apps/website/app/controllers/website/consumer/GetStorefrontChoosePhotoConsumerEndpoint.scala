b5cd2c45130c7871402eba81d7f0755493082621
Erem Boto <ehboto@gmail.com>
2012-06-27 19:06:11 -0700

    Makes a type-safe version of the play session and uses it to populate header and footer data in new website controllers. SER-48

--- apps/website/app/controllers/website/consumer/GetStorefrontChoosePhotoConsumerEndpoint.scala
+++ apps/website/app/controllers/website/consumer/GetStorefrontChoosePhotoConsumerEndpoint.scala
@@ -6,20 +6,20 @@ import play.mvc.Controller
 /**
  * Endpoint for serving up the Choose Photo page
  */
-private [consumer] trait GetStorefrontChoosePhotoConsumerEndpoint { this: Controller =>
+private[consumer] trait GetStorefrontChoosePhotoConsumerEndpoint { this: Controller =>
   protected def controllerMethod: ControllerMethod
   protected def celebFilters: CelebrityAccountRequestFilters
 
   def getStorefrontChoosePhotoTiled(celebrityUrlSlug: String, productUrlSlug: String) = controllerMethod()
   {
     celebFilters.requireCelebrityAndProductUrlSlugs { (celebrity, product) =>
-
+      // Fill this with actual rendering of the choose photo page.
     }
   }
 }
 
-object getStorefrontChoosePhotoConsumerEndpoint {
-  object ChoosePhotoViewingOption = {
-
+private[consumer] object GetStorefrontChoosePhotoConsumerEndpoint {
+  object ChoosePhotoViewingOption {
+    // Fill this with portrait and landscape
   }
 }
