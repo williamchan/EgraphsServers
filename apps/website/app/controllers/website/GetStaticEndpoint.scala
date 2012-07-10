cfef603657db6f9d8107fd6f5862bef3c456fee4
David Auld <dhauld@gmail.com>
2012-07-10 17:00:26 -0400

    More copy edits, this time hopefully without a #mergefail

--- apps/website/app/controllers/website/GetStaticEndpoint.scala
+++ apps/website/app/controllers/website/GetStaticEndpoint.scala
@@ -24,7 +24,7 @@ private[controllers] trait GetStaticEndpoint extends ImplicitHeaderAndFooterData
         Section(title="Introduction", url="#inside", subsection = None),
         Section(title="What is an egraph?", url="#what", subsection = None),
         Section(title="The biometric authentication process", url="#biometric", subsection = None),
-        Section(title="Sharing an egraph", url="#do", subsection = None)
+        Section(title="Enjoying and sharing your egraph", url="#do", subsection = None)
       )
     views.frontend.html.inside_egraph(tableOfContents)
   }
