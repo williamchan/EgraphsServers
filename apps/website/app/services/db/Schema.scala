31df980941eaf1723153ee847fb5c598b6f1314a
William Chan <will@egraphs.com>
2012-08-01 17:58:41 -0700

    Organize and alphabetize Schema.scala. Hooray!

--- apps/website/app/services/db/Schema.scala
+++ apps/website/app/services/db/Schema.scala
@@ -28,8 +28,12 @@ class Schema @Inject()(
   import uk.me.lings.scalaguice.InjectorExtensions._
 
   //
-  // Addresses
+  // table declarations -- please keep these alphabetized
   //
+
+  val accounts = table[Account]
+  on(accounts)(account => declare(account.email is unique))
+
   val addresses = table[Address]
   on(addresses)(address =>
     declare(
@@ -38,15 +42,19 @@ class Schema @Inject()(
     )
   )
 
-  //
-  // Customers
-  //
-  val customers = table[Customer]
-  on(customers)(customer => declare(customer.username is unique))
+  val administrators = table[Administrator]
 
-  //
-  // Celebrities
-  //
+  val blobKeys = table[BlobKey]
+  on(blobKeys)(blobKey =>
+    declare(
+      blobKey.key is (unique),
+      blobKey.url is dbType("varchar(255)")
+    )
+  )
+
+  val cashTransactions = table[CashTransaction]
+  on(cashTransactions)(cashTransaction => declare(cashTransaction.amountInCurrency is monetaryDbType))
+
   val celebrities = table[Celebrity]
   on(celebrities)(celebrity =>
     declare(
@@ -56,48 +64,32 @@ class Schema @Inject()(
     )
   )
 
-  //
-  // Administrators
-  //
-  val administrators = table[Administrator]
+  val customers = table[Customer]
+  on(customers)(customer => declare(customer.username is unique))
 
-  //
-  // Cash transactions
-  //
-  val cashTransactions = table[CashTransaction]
-  on(cashTransactions)(cashTransaction => declare(cashTransaction.amountInCurrency is monetaryDbType))
-  //
-  // Accounts
-  //
-  val accounts = table[Account]
-  on(accounts)(account => declare(account.email is unique))
+  val egraphs = table[Egraph]
+  on(egraphs)(egraph => declare(columns(egraph.orderId, egraph._egraphState) are indexed))
 
-  val accountToCustomer = oneAccountPerRowOn(customers, (account) => account.customerId)
-  val accountToAdministrator = oneAccountPerRowOn(administrators, (acct) => acct.administratorId)
-  val accountToCelebrity = oneAccountPerRowOn(celebrities, (account) => account.celebrityId)
-  val accountToTransaction = oneToManyRelation(accounts, cashTransactions).via(
-    (account, cashTransaction) => account.id === cashTransaction.accountId)
-  val accountToAddress = oneToManyRelation(accounts, addresses).via(
-    (account, address) => account.id === address.accountId)
-
-
-  //
-  // Products
-  //
-  val products = table[Product]
-  on(products)(product =>
+  val enrollmentBatches = table[EnrollmentBatch]
+  on(enrollmentBatches)(enrollmentBatch =>
     declare(
-      product.priceInCurrency is monetaryDbType,
-      product.storyText is dbType("text"),
-      columns(product.celebrityId, product.urlSlug) are (unique)
+      columns(enrollmentBatch.celebrityId, enrollmentBatch.isBatchComplete, enrollmentBatch.isSuccessfulEnrollment) are indexed
     )
   )
-  val celebrityToProduct = oneToManyRelation(celebrities, products).
-    via((celebrity, product) => celebrity.id === product.celebrityId)
 
-  //
-  // Orders
-  //
+  val enrollmentSamples = table[EnrollmentSample]
+
+  val failedPurchaseData = table[FailedPurchaseData]
+  on(failedPurchaseData)(datum => declare( datum.purchaseData is (dbType("varchar(1000)")) ))
+
+  val inventoryBatches = table[InventoryBatch]
+  on(inventoryBatches)(inventoryBatch =>
+    declare(
+      columns(inventoryBatch.startDate, inventoryBatch.endDate) are (indexed),
+      columns(inventoryBatch.celebrityId, inventoryBatch.startDate, inventoryBatch.endDate) are (indexed)
+    )
+  )
+
   val orders = table[Order]("Orders")
   on(orders)(order =>
     declare(
@@ -109,49 +101,49 @@ class Schema @Inject()(
     )
   )
 
-  val productToOrders = oneToManyRelation(products, orders)
-    .via((product, order) => product.id === order.productId)
-  productToOrders.foreignKeyDeclaration.constrainReference(onDelete cascade)
-
-  val buyingCustomerToOrders = oneToManyRelation(customers, orders)
-    .via((customer, order) => customer.id === order.buyerId)
-  buyingCustomerToOrders.foreignKeyDeclaration.constrainReference(onDelete cascade)
-
-  val recipientCustomerToOrders = oneToManyRelation(customers, orders)
-    .via((customer, order) => customer.id === order.recipientId)
-  recipientCustomerToOrders.foreignKeyDeclaration.constrainReference(onDelete cascade)
-
-  //
-  // PrintOrder
-  //
   val printOrders = table[PrintOrder]
-  on(printOrders)(printOrder => declare(printOrder.shippingAddress is (dbType("varchar(255)"))))
-  val orderToPrintOrders = oneToManyRelation(orders, printOrders)
-    .via((order, printOrder) => order.id === printOrder.orderId)
-  orderToPrintOrders.foreignKeyDeclaration.constrainReference(onDelete cascade)
+  on(printOrders)(printOrder => declare(
+    printOrder.shippingAddress is (dbType("varchar(255)")))
+  )
 
-  //
-  // Egraphs
-  //
-  val egraphs = table[Egraph]
-  on(egraphs)(egraph => declare(columns(egraph.orderId, egraph._egraphState) are indexed))
-
-  val orderToEgraphs = oneToManyRelation(orders, egraphs)
-    .via((order, egraph) => order.id === egraph.orderId)
-  orderToEgraphs.foreignKeyDeclaration.constrainReference(onDelete cascade)
-
-  //
-  // InventoryBatch
-  //
-  val inventoryBatches = table[InventoryBatch]
-  val celebrityToInventoryBatches = oneToManyRelation(celebrities, inventoryBatches).
-    via((celebrity, inventoryBatch) => celebrity.id === inventoryBatch.celebrityId)
-  on(inventoryBatches)(inventoryBatch =>
+  val products = table[Product]
+  on(products)(product =>
     declare(
-      columns(inventoryBatch.startDate, inventoryBatch.endDate) are (indexed),
-      columns(inventoryBatch.celebrityId, inventoryBatch.startDate, inventoryBatch.endDate) are (indexed)
+      product.priceInCurrency is monetaryDbType,
+      product.storyText is dbType("text"),
+      columns(product.celebrityId, product.urlSlug) are (unique)
     )
   )
+
+  // ugh, why did I make so many biometrics tables?
+  val vbgAudioCheckTable = table[VBGAudioCheck]
+  val vbgEnrollUserTable = table[VBGEnrollUser]
+  val vbgFinishEnrollTransactionTable = table[VBGFinishEnrollTransaction]
+  val vbgFinishVerifyTransactionTable = table[VBGFinishVerifyTransaction]
+  val vbgStartEnrollmentTable = table[VBGStartEnrollment]
+  val vbgStartVerificationTable = table[VBGStartVerification]
+  val vbgVerifySampleTable = table[VBGVerifySample]
+  val xyzmoAddProfileTable = table[XyzmoAddProfile]
+  on(xyzmoAddProfileTable)(xyzmoAddProfile => declare(xyzmoAddProfile.errorMsg is dbType("varchar(255)")))
+  val xyzmoAddUserTable = table[XyzmoAddUser]
+  on(xyzmoAddUserTable)(xyzmoAddUser => declare(xyzmoAddUser.errorMsg is dbType("varchar(255)")))
+  val xyzmoDeleteUserTable = table[XyzmoDeleteUser]
+  on(xyzmoDeleteUserTable)(xyzmoDeleteUser => declare(xyzmoDeleteUser.errorMsg is dbType("varchar(255)")))
+  val xyzmoEnrollDynamicProfileTable = table[XyzmoEnrollDynamicProfile]
+  on(xyzmoEnrollDynamicProfileTable)(xyzmoEnrollDynamicProfile =>
+    declare(
+      xyzmoEnrollDynamicProfile.errorMsg is (dbType("varchar(255)")),
+      xyzmoEnrollDynamicProfile.rejectedSignaturesSummary is (dbType("varchar(255)")),
+      xyzmoEnrollDynamicProfile.enrollmentSampleIds is (dbType("varchar(255)"))
+    )
+  )
+  val xyzmoVerifyUserTable = table[XyzmoVerifyUser]
+  on(xyzmoVerifyUserTable)(xyzmoVerifyUser => declare(xyzmoVerifyUser.errorMsg is dbType("varchar(255)")))
+
+
+  //
+  // manyToManyRelation declarations -- please keep these alphabetized
+  //
 
   val inventoryBatchProducts = manyToManyRelation(inventoryBatches, products)
     .via[InventoryBatchProduct]((inventoryBatch, product, join) => (join.inventoryBatchId === inventoryBatch.id, join.productId === product.id))
@@ -161,117 +153,92 @@ class Schema @Inject()(
     )
   )
 
-  val inventoryBatchToOrders = oneToManyRelation(inventoryBatches, orders)
-    .via((inventoryBatch, order) => inventoryBatch.id === order.inventoryBatchId)
+  //
+  // oneToManyRelation declarations -- please keep these organized
+  //
 
-  //
-  // Biometrics
-  //
-  val enrollmentBatches = table[EnrollmentBatch]
+  val accountToAdministrator = oneAccountPerRowOn(administrators, (acct) => acct.administratorId)
+  val accountToCelebrity = oneAccountPerRowOn(celebrities, (account) => account.celebrityId)
+  val accountToCustomer = oneAccountPerRowOn(customers, (account) => account.customerId)
+  val accountToAddress = oneToManyRelation(accounts, addresses).via(
+    (account, address) => account.id === address.accountId)
+  val accountToTransaction = oneToManyRelation(accounts, cashTransactions).via(
+    (account, cashTransaction) => account.id === cashTransaction.accountId)
+
   val celebrityToEnrollmentBatches = oneToManyRelation(celebrities, enrollmentBatches)
     .via((celebrity, enrollmentBatch) => celebrity.id === enrollmentBatch.celebrityId)
   celebrityToEnrollmentBatches.foreignKeyDeclaration.constrainReference(onDelete cascade)
-  on(enrollmentBatches)(enrollmentBatch =>
-    declare(
-      columns(enrollmentBatch.celebrityId, enrollmentBatch.isBatchComplete, enrollmentBatch.isSuccessfulEnrollment) are indexed
-    )
-  )
+  val celebrityToProduct = oneToManyRelation(celebrities, products).
+    via((celebrity, product) => celebrity.id === product.celebrityId)
+  val celebrityToInventoryBatches = oneToManyRelation(celebrities, inventoryBatches).
+    via((celebrity, inventoryBatch) => celebrity.id === inventoryBatch.celebrityId)
 
-  val enrollmentSamples = table[EnrollmentSample]
+  val buyingCustomerToOrders = oneToManyRelation(customers, orders)
+    .via((customer, order) => customer.id === order.buyerId)
+  buyingCustomerToOrders.foreignKeyDeclaration.constrainReference(onDelete cascade)
+  val recipientCustomerToOrders = oneToManyRelation(customers, orders)
+    .via((customer, order) => customer.id === order.recipientId)
+  recipientCustomerToOrders.foreignKeyDeclaration.constrainReference(onDelete cascade)
+
   val enrollmentBatchToEnrollmentSamples = oneToManyRelation(enrollmentBatches, enrollmentSamples)
     .via((enrollmentBatch, enrollmentSample) => enrollmentBatch.id === enrollmentSample.enrollmentBatchId)
   enrollmentBatchToEnrollmentSamples.foreignKeyDeclaration.constrainReference(onDelete cascade)
 
+  val inventoryBatchToOrders = oneToManyRelation(inventoryBatches, orders)
+    .via((inventoryBatch, order) => inventoryBatch.id === order.inventoryBatchId)
 
-  val vbgAudioCheckTable = table[VBGAudioCheck]
+  val orderToEgraphs = oneToManyRelation(orders, egraphs)
+    .via((order, egraph) => order.id === egraph.orderId)
+  orderToEgraphs.foreignKeyDeclaration.constrainReference(onDelete cascade)
+
+  val orderToPrintOrders = oneToManyRelation(orders, printOrders)
+    .via((order, printOrder) => order.id === printOrder.orderId)
+  orderToPrintOrders.foreignKeyDeclaration.constrainReference(onDelete cascade)
+
+  val productToOrders = oneToManyRelation(products, orders)
+    .via((product, order) => product.id === order.productId)
+  productToOrders.foreignKeyDeclaration.constrainReference(onDelete cascade)
+
+
+  // ugh, why did I make so many biometrics tables?
   val enrollmentBatchToVBGAudioCheckTable = oneToManyRelation(enrollmentBatches, vbgAudioCheckTable)
     .via((enrollmentBatch, vbgAudioCheck) => enrollmentBatch.id === vbgAudioCheck.enrollmentBatchId)
   enrollmentBatchToVBGAudioCheckTable.foreignKeyDeclaration.constrainReference(onDelete cascade)
-
-  val vbgEnrollUserTable = table[VBGEnrollUser]
   val enrollmentBatchToVBGEnrollUserTable = oneToManyRelation(enrollmentBatches, vbgEnrollUserTable)
     .via((enrollmentBatch, vbgEnrollUser) => enrollmentBatch.id === vbgEnrollUser.enrollmentBatchId)
   enrollmentBatchToVBGEnrollUserTable.foreignKeyDeclaration.constrainReference(onDelete cascade)
-
-  val vbgFinishEnrollTransactionTable = table[VBGFinishEnrollTransaction]
   val enrollmentBatchToVBGFinishEnrollTransactionTable = oneToManyRelation(enrollmentBatches, vbgFinishEnrollTransactionTable)
     .via((enrollmentBatch, vbgFinishEnrollTransaction) => enrollmentBatch.id === vbgFinishEnrollTransaction.enrollmentBatchId)
   enrollmentBatchToVBGFinishEnrollTransactionTable.foreignKeyDeclaration.constrainReference(onDelete cascade)
-
-  val vbgStartEnrollmentTable = table[VBGStartEnrollment]
-  val enrollmentBatchToVBGStartEnrollmentTable = oneToManyRelation(enrollmentBatches, vbgStartEnrollmentTable)
-    .via((enrollmentBatch, vbgStartEnrollment) => enrollmentBatch.id === vbgStartEnrollment.enrollmentBatchId)
-  enrollmentBatchToVBGStartEnrollmentTable.foreignKeyDeclaration.constrainReference(onDelete cascade)
-
-  val vbgFinishVerifyTransactionTable = table[VBGFinishVerifyTransaction]
   val egraphToVBGFinishVerifyTransactionTable = oneToManyRelation(egraphs, vbgFinishVerifyTransactionTable)
     .via((egraph, vbgFinishVerifyTransaction) => egraph.id === vbgFinishVerifyTransaction.egraphId)
   egraphToVBGFinishVerifyTransactionTable.foreignKeyDeclaration.constrainReference(onDelete cascade)
-
-  val vbgStartVerificationTable = table[VBGStartVerification]
+  val enrollmentBatchToVBGStartEnrollmentTable = oneToManyRelation(enrollmentBatches, vbgStartEnrollmentTable)
+    .via((enrollmentBatch, vbgStartEnrollment) => enrollmentBatch.id === vbgStartEnrollment.enrollmentBatchId)
+  enrollmentBatchToVBGStartEnrollmentTable.foreignKeyDeclaration.constrainReference(onDelete cascade)
   val egraphToVBGStartVerificationTable = oneToManyRelation(egraphs, vbgStartVerificationTable)
     .via((egraph, vbgStartVerification) => egraph.id === vbgStartVerification.egraphId)
   egraphToVBGStartVerificationTable.foreignKeyDeclaration.constrainReference(onDelete cascade)
-
-  val vbgVerifySampleTable = table[VBGVerifySample]
   val egraphToVBGVerifySampleTable = oneToManyRelation(egraphs, vbgVerifySampleTable)
     .via((egraph, vbgVerifySample) => egraph.id === vbgVerifySample.egraphId)
   egraphToVBGVerifySampleTable.foreignKeyDeclaration.constrainReference(onDelete cascade)
 
-
-  val xyzmoDeleteUserTable = table[XyzmoDeleteUser]
-  val enrollmentBatchToXyzmoDeleteUserTable = oneToManyRelation(enrollmentBatches, xyzmoDeleteUserTable)
-    .via((enrollmentBatch, xyzmoDeleteUser) => enrollmentBatch.id === xyzmoDeleteUser.enrollmentBatchId)
-  enrollmentBatchToXyzmoDeleteUserTable.foreignKeyDeclaration.constrainReference(onDelete cascade)
-  on(xyzmoDeleteUserTable)(xyzmoDeleteUser => declare(xyzmoDeleteUser.errorMsg is dbType("varchar(255)")))
-
-  val xyzmoAddUserTable = table[XyzmoAddUser]
-  val enrollmentBatchToXyzmoAddUserTable = oneToManyRelation(enrollmentBatches, xyzmoAddUserTable)
-    .via((enrollmentBatch, xyzmoAddUser) => enrollmentBatch.id === xyzmoAddUser.enrollmentBatchId)
-  enrollmentBatchToXyzmoAddUserTable.foreignKeyDeclaration.constrainReference(onDelete cascade)
-  on(xyzmoAddUserTable)(xyzmoAddUser => declare(xyzmoAddUser.errorMsg is dbType("varchar(255)")))
-
-  val xyzmoAddProfileTable = table[XyzmoAddProfile]
   val enrollmentBatchToXyzmoAddProfileTable = oneToManyRelation(enrollmentBatches, xyzmoAddProfileTable)
     .via((enrollmentBatch, xyzmoAddProfile) => enrollmentBatch.id === xyzmoAddProfile.enrollmentBatchId)
   enrollmentBatchToXyzmoAddProfileTable.foreignKeyDeclaration.constrainReference(onDelete cascade)
-  on(xyzmoAddProfileTable)(xyzmoAddProfile => declare(xyzmoAddProfile.errorMsg is dbType("varchar(255)")))
-
-  val xyzmoEnrollDynamicProfileTable = table[XyzmoEnrollDynamicProfile]
+  val enrollmentBatchToXyzmoAddUserTable = oneToManyRelation(enrollmentBatches, xyzmoAddUserTable)
+    .via((enrollmentBatch, xyzmoAddUser) => enrollmentBatch.id === xyzmoAddUser.enrollmentBatchId)
+  enrollmentBatchToXyzmoAddUserTable.foreignKeyDeclaration.constrainReference(onDelete cascade)
+  val enrollmentBatchToXyzmoDeleteUserTable = oneToManyRelation(enrollmentBatches, xyzmoDeleteUserTable)
+    .via((enrollmentBatch, xyzmoDeleteUser) => enrollmentBatch.id === xyzmoDeleteUser.enrollmentBatchId)
+  enrollmentBatchToXyzmoDeleteUserTable.foreignKeyDeclaration.constrainReference(onDelete cascade)
   val enrollmentBatchToXyzmoEnrollDynamicProfileTable = oneToManyRelation(enrollmentBatches, xyzmoEnrollDynamicProfileTable)
     .via((enrollmentBatch, xyzmoEnrollDynamicProfile) => enrollmentBatch.id === xyzmoEnrollDynamicProfile.enrollmentBatchId)
   enrollmentBatchToXyzmoEnrollDynamicProfileTable.foreignKeyDeclaration.constrainReference(onDelete cascade)
-  on(xyzmoEnrollDynamicProfileTable)(xyzmoEnrollDynamicProfile =>
-    declare(
-      xyzmoEnrollDynamicProfile.errorMsg is (dbType("varchar(255)")),
-      xyzmoEnrollDynamicProfile.rejectedSignaturesSummary is (dbType("varchar(255)")),
-      xyzmoEnrollDynamicProfile.enrollmentSampleIds is (dbType("varchar(255)"))
-
-    )
-  )
-
-  val xyzmoVerifyUserTable = table[XyzmoVerifyUser]
   val egraphToXyzmoVerifyUserTable = oneToManyRelation(egraphs, xyzmoVerifyUserTable)
     .via((egraph, xyzmoVerifyUser) => egraph.id === xyzmoVerifyUser.egraphId)
   egraphToXyzmoVerifyUserTable.foreignKeyDeclaration.constrainReference(onDelete cascade)
-  on(xyzmoVerifyUserTable)(xyzmoVerifyUser => declare(xyzmoVerifyUser.errorMsg is dbType("varchar(255)")))
 
-  //
-  // FailedPurchaseData
-  //
-  val failedPurchaseData = table[FailedPurchaseData]
-  on(failedPurchaseData)(datum => declare( datum.purchaseData is (dbType("varchar(1000)")) ))
-
-  //
-  // BlobKeys
-  //
-  val blobKeys = table[BlobKey]
-  on(blobKeys)(blobKey =>
-    declare(
-      blobKey.key is (unique),
-      blobKey.url is dbType("varchar(255)")
-    )
-  )
 
   //
   // Public methods
@@ -400,11 +367,11 @@ class Schema @Inject()(
     Seq(
       factoryFor(accounts) is Account(services = injector.instance[AccountServices]),
       factoryFor(addresses) is Address(services = injector.instance[AddressServices]),
+      factoryFor(administrators) is Administrator(services = injector.instance[AdministratorServices]),
       factoryFor(blobKeys) is BlobKey(services = injector.instance[BlobKeyServices]),
       factoryFor(cashTransactions) is CashTransaction(services = injector.instance[CashTransactionServices]),
       factoryFor(celebrities) is Celebrity(services = injector.instance[CelebrityServices]),
       factoryFor(customers) is Customer(services = injector.instance[CustomerServices]),
-      factoryFor(administrators) is Administrator(services = injector.instance[AdministratorServices]),
       factoryFor(egraphs) is Egraph(services = injector.instance[EgraphServices]),
       factoryFor(enrollmentBatches) is EnrollmentBatch(services = injector.instance[EnrollmentBatchServices]),
       factoryFor(enrollmentSamples) is EnrollmentSample(services = injector.instance[EnrollmentSampleServices]),
@@ -421,9 +388,9 @@ class Schema @Inject()(
       factoryFor(vbgStartEnrollmentTable) is VBGStartEnrollment(services = injector.instance[VBGStartEnrollmentServices]),
       factoryFor(vbgStartVerificationTable) is VBGStartVerification(services = injector.instance[VBGStartVerificationServices]),
       factoryFor(vbgVerifySampleTable) is VBGVerifySample(services = injector.instance[VBGVerifySampleServices]),
+      factoryFor(xyzmoAddProfileTable) is XyzmoAddProfile(services = injector.instance[XyzmoAddProfileServices]),
       factoryFor(xyzmoAddUserTable) is XyzmoAddUser(services = injector.instance[XyzmoAddUserServices]),
       factoryFor(xyzmoDeleteUserTable) is XyzmoDeleteUser(services = injector.instance[XyzmoDeleteUserServices]),
-      factoryFor(xyzmoAddProfileTable) is XyzmoAddProfile(services = injector.instance[XyzmoAddProfileServices]),
       factoryFor(xyzmoEnrollDynamicProfileTable) is XyzmoEnrollDynamicProfile(services = injector.instance[XyzmoEnrollDynamicProfileServices]),
       factoryFor(xyzmoVerifyUserTable) is XyzmoVerifyUser(services = injector.instance[XyzmoVerifyUserServices])
     )
