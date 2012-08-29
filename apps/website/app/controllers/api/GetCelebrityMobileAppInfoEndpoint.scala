e1a83f1bcff803f602f5f14856891433cc9c6e0d
William Chan <will@egraphs.com>
2012-08-29 10:32:39 -0700

    Minor changes.

--- apps/website/app/controllers/api/GetCelebrityMobileAppInfoEndpoint.scala
+++ apps/website/app/controllers/api/GetCelebrityMobileAppInfoEndpoint.scala
@@ -12,16 +12,13 @@ private[controllers] trait GetCelebrityMobileAppInfoEndpoint { this: Controller 
   protected def celebFilters: CelebrityAccountRequestFilters
   protected def blobs: Blobs
 
+  // These values can be persisted when we have need to support more than one version of the iPad app.
+  private val version = "1_2_3_11"
+  private val s3Key = "ipad/1_2_3_11/Egraphs.ipa"
+
   def getCelebrityMobileAppInfo = controllerMethod() {
     celebFilters.requireCelebrityAccount { (account, celebrity) =>
-
-      // Need a table to track what the most-recent version of the iPad app is.
-      // Table called MobileAppVersion with columns: Device, Version, Key.
-      // Celebrity gets a mobileAppVersionId FK. None if this API should return the latest. Else, point to a specific MobileAppVersion.
-      val version = "1_2_3_11"
-      val key = "ipad/1_2_3_11/Egraphs.ipa"
-
-      val ipaUrl = blobs.getStaticResourceUrl(key, 1.hour)
+      val ipaUrl = blobs.getStaticResourceUrl(s3Key, 10.minutes)
       val iPadAppInfo = Map("version" -> version, "ipaURL" -> ipaUrl)
       val mobileAppInfo = Map("ipad" -> iPadAppInfo)
       Serializer.SJSON.toJSON(mobileAppInfo)
