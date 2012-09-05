d4588ab9c7f8ff2db78af6e32f1fb5f041dc10fe
William Chan <will@egraphs.com>
2012-09-04 23:33:41 -0700

    SER-247: Enable admins to change shipping addresses

--- apps/website/app/controllers/website/admin/GetPrintOrderAdminEndpoint.scala
+++ apps/website/app/controllers/website/admin/GetPrintOrderAdminEndpoint.scala
@@ -5,6 +5,7 @@ import services.http.{AdminRequestFilters, ControllerMethod}
 import models._
 import scala.Some
 import controllers.WebsiteControllers
+import org.apache.commons.lang.StringEscapeUtils
 
 private[controllers] trait GetPrintOrderAdminEndpoint { this: Controller =>
 
@@ -19,11 +20,22 @@ private[controllers] trait GetPrintOrderAdminEndpoint { this: Controller =>
     adminFilters.requireAdministratorLogin { admin =>
       printOrderStore.findById(printOrderId) match {
         case Some(printOrder) => {
-          printOrder
           val order = orderStore.get(printOrder.orderId)
           val recipientEmail = order.recipient.account.email
           val egraph = egraphStore.findByOrder(printOrder.orderId, egraphQueryFilters.publishedOrApproved).headOption
-          views.Application.admin.html.admin_printorder(printOrder = printOrder, order = order, recipientEmail = recipientEmail, egraph = egraph)
+
+          val fieldDefaults: (String => String) = {
+            (paramName: String) => paramName match {
+              case "shippingAddress" => StringEscapeUtils.escapeHtml(printOrder.shippingAddress)
+            }
+          }
+
+          views.Application.admin.html.admin_printorder(
+            printOrder = printOrder,
+            order = order,
+            recipientEmail = recipientEmail,
+            egraph = egraph,
+            fields = fieldDefaults)
         }
         case None => NotFound("Print Order with id " + printOrderId.toString + " not found")
       }
