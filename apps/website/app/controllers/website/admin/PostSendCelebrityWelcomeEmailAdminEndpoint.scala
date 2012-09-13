7c4f2eb12f8bb84570b56f2a0ed225822ae5296f
Andrew B. Smith <drewsmits@gmail.com>
2012-09-13 16:37:23 -0700

    [SER-282] Added send welcome email endpoint

---
+++ apps/website/app/controllers/website/admin/PostSendCelebrityWelcomeEmailAdminEndpoint.scala
@@ -0,0 +1,52 @@
+package controllers.website.admin
+
+import play.data.validation._
+import models._
+import enums.PublishedStatus
+import play.mvc.results.Redirect
+import services.mail.TransactionalMail
+import controllers.WebsiteControllers
+import play.mvc.Controller
+import play.data.validation.Validation.ValidationResult
+import services.blobs.Blobs.Conversions._
+import java.io.File
+import services.{ImageUtil, Utils}
+import services.http.{POSTControllerMethod, AdminRequestFilters, CelebrityAccountRequestFilters}
+import org.apache.commons.mail.HtmlEmail
+
+trait PostSendCelebrityWelcomeEmailAdminEndpoint {
+  this: Controller =>
+
+  protected def postController: POSTControllerMethod
+  protected def adminFilters: AdminRequestFilters
+  protected def celebrityStore: CelebrityStore
+  protected def accountStore: AccountStore
+  protected def transactionalMail: TransactionalMail
+
+  def postSendCelebrityWelcomeEmailAdmin(celebrityId: Long) = postController() {
+    adminFilters.requireAdministratorLogin { admin =>      
+      val maybeSuccessfulRedirect = for (
+        celebrity <- celebrityStore.findById(celebrityId);
+        account <- accountStore.findByCelebrityId(celebrityId)
+      ) yield {
+        val email = new HtmlEmail()
+
+        val html = views.frontend.html.celebrity_welcome_email(
+          celebrityName = celebrity.publicName,
+          celebrityEmail = account.email
+        )
+
+        email.setFrom("noreply@egraphs.com", "Egraphs")
+        email.addTo(account.email, celebrity.publicName)
+        email.setSubject("Welcome to Egraphs")
+        email.setHtmlMsg(html.toString())
+
+        transactionalMail.send(email)
+  
+        WebsiteControllers.redirectWithValidationErrors(GetCelebrityAdminEndpoint.url(celebrityId = celebrityId))
+      }
+
+      maybeSuccessfulRedirect.getOrElse(NotFound("Celebrity with Id " + celebrityId + " not NotFound"))
+    }
+  }
+}
