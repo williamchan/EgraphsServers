da268904600971456808f74d9e4ff483a385f7f8
myyk <myyk@egraphs.com>
2012-09-14 13:27:28 -0700

    The beginning of bringing in the website controllers and endpoints.  Everything is commented out and few files moved over.

---
+++ apps/website-2/app/controllers/website/AllWebsiteEndpoints.scala
@@ -0,0 +1,16 @@
+package controllers.website
+
+import play.api.mvc.Controller
+//import admin.AllAdminEndpoints
+//import consumer.{GetRootConsumerEndpoint, AllConsumerEndpoints}
+//import controllers.website.nonproduction.PostBuyDemoProductEndpoint
+
+/**
+ * All endpoints for the website (doesn't include the API)
+ */
+trait AllWebsiteEndpoints
+//  extends GetRootConsumerEndpoint
+//  with AllConsumerEndpoints
+//  with AllAdminEndpoints
+//  with PostBuyDemoProductEndpoint
+{ this: Controller =>}
