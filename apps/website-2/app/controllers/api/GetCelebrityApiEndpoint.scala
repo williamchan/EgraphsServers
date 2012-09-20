37f7e9f93224c0787a7bd052b4f3cdb424a970ce
myyk <myyk@egraphs.com>
2012-09-19 19:21:16 -0700

    Actually we don't need to do that with Json, using Serializer.SJson.toJSON instead.

--- apps/website-2/app/controllers/api/GetCelebrityApiEndpoint.scala
+++ apps/website-2/app/controllers/api/GetCelebrityApiEndpoint.scala
@@ -1,12 +1,8 @@
 package controllers.api
 
 import play.api.mvc._
+import sjson.json.Serializer
 import services.http.{ControllerMethod, CelebrityAccountRequestFilters}
-import play.api.libs.json.Json
-import play.api.libs.json.Json.toJson
-import models.Celebrity
-import play.api.libs.json.JsValue
-import services.Time
 
 private[controllers] trait GetCelebrityApiEndpoint { this: Controller =>
   protected def controllerMethod: ControllerMethod
@@ -20,29 +16,8 @@ private[controllers] trait GetCelebrityApiEndpoint { this: Controller =>
   def getCelebrity = Action { implicit request =>
     controllerMethod() {
       celebFilters.requireCelebrityAccount { (account, celebrity) =>
-        Ok(celebrityToJson(celebrity))
+        Ok(Serializer.SJSON.toJSON(celebrity.renderedForApi))
       }
     }
-  }
-
-  /**
-   * Renders the Celebrity as a Map, which will itself be rendered into whichever data format
-   * by the API (e.g. JSON)
-   */
-  // maybe we need a toJson Helper object
-  def celebrityToJson(celebrity: Celebrity): JsValue = {
-    toJson(
-      Map(
-        "id" -> toJson(celebrity.id),
-        "enrollmentStatus" -> toJson(celebrity.enrollmentStatus.name),
-        "publicName" -> toJson(celebrity.publicName),
-        "urlSlug" -> toJson(celebrity.urlSlug)
-      ) ++
-      // these could be for any HasCreatedUpdated object
-      Map(
-        "created" -> toJson(Time.toApiFormat(celebrity.created)),
-        "updated" -> toJson(Time.toApiFormat(celebrity.updated))
-      )
-    )
   }
 }
