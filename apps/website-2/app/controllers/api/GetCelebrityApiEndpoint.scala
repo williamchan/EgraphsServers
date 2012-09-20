6b291f1a87af5b9fb1513e0bde9f52e24822a905
myyk <myyk@egraphs.com>
2012-09-19 19:13:40 -0700

    Rendered the celebrity as Json, this should be moved though.

--- apps/website-2/app/controllers/api/GetCelebrityApiEndpoint.scala
+++ apps/website-2/app/controllers/api/GetCelebrityApiEndpoint.scala
@@ -1,8 +1,12 @@
 package controllers.api
 
-import play.api.mvc.Controller
-import sjson.json.Serializer
+import play.api.mvc._
 import services.http.{ControllerMethod, CelebrityAccountRequestFilters}
+import play.api.libs.json.Json
+import play.api.libs.json.Json.toJson
+import models.Celebrity
+import play.api.libs.json.JsValue
+import services.Time
 
 private[controllers] trait GetCelebrityApiEndpoint { this: Controller =>
   protected def controllerMethod: ControllerMethod
@@ -13,9 +17,32 @@ private[controllers] trait GetCelebrityApiEndpoint { this: Controller =>
    *
    * See [[https://egraphs.jira.com/wiki/display/DEV/API+Endpoints#APIEndpoints-Celebrities%C2%A0Celebrities the json spec]].
    */
-  def getCelebrity = controllerMethod() {
-    celebFilters.requireCelebrityAccount { (account, celebrity) =>
-      Serializer.SJSON.toJSON(celebrity.renderedForApi)
+  def getCelebrity = Action { implicit request =>
+    controllerMethod() {
+      celebFilters.requireCelebrityAccount { (account, celebrity) =>
+        Ok(celebrityToJson(celebrity))
+      }
     }
+  }
+
+  /**
+   * Renders the Celebrity as a Map, which will itself be rendered into whichever data format
+   * by the API (e.g. JSON)
+   */
+  // maybe we need a toJson Helper object
+  def celebrityToJson(celebrity: Celebrity): JsValue = {
+    toJson(
+      Map(
+        "id" -> toJson(celebrity.id),
+        "enrollmentStatus" -> toJson(celebrity.enrollmentStatus.name),
+        "publicName" -> toJson(celebrity.publicName),
+        "urlSlug" -> toJson(celebrity.urlSlug)
+      ) ++
+      // these could be for any HasCreatedUpdated object
+      Map(
+        "created" -> toJson(Time.toApiFormat(celebrity.created)),
+        "updated" -> toJson(Time.toApiFormat(celebrity.updated))
+      )
+    )
   }
 }
