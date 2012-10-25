27aa54320d938a78db80fef6663432e5687c6725
Erem Boto <ehboto@gmail.com>
2012-10-24 23:33:50 -0700

    SER-431. Fixes favicon and celebrity agreement routes.

--- tools/source_directories.sh
+++ tools/source_directories.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $SCRIPT_DIR/..
 
-find -E . -regex '.*(/app|/main|/test)' \
+find -E . -regex '.*(/app|/main|/test|/conf)' \
   | grep -v 'target' \
   | egrep -v '(\.git|tmp|svgweb|jasmine|less)' 
