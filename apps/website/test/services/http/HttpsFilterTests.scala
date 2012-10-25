0d125d98da9553569fe105acf9a4c23882e027a6
Erem Boto <ehboto@gmail.com>
2012-10-24 22:57:14 -0700

    SER-428. Fixes test cases

--- apps/website/test/services/http/HttpsFilterTests.scala
+++ apps/website/test/services/http/HttpsFilterTests.scala
@@ -5,44 +5,40 @@ import play.api.mvc.Results._
 import play.api.test.FakeRequest
 import utils.EgraphsUnitTest
 import services.config.ConfigFileProxy
+import play.api.test.Helpers._
 
-//TODO: PLAY20 - having trouble getting the logic in HttpsFilter to actually execute in these tests
 class HttpsFilterTests extends EgraphsUnitTest {
 
-  "HttpsFilter" should "redirect insecure http requests to https when httpsOnly is true" in (pending)
-
-  "HttpsFilter" should "serve https requests" in (pending)
-
-  "HttpsFilter" should "serve http requests when httpsOnly is false" in (pending)
-  
-//  "HttpsFilter" should "redirect insecure http requests to https when httpsOnly is true" in {
-//    val request: Request[AnyContent] = FakeRequest("GET", "www.egraphs.com").withHeaders("x-forwarded-proto" -> "http")
-//    val mockConfig = mock[ConfigFileProxy]
-//    mockConfig.applicationHttpsOnly returns true
-//    
-//    val httpsFilter = new HttpsFilter(mockConfig)
-//    val action = httpsFilter(Action { request => Ok } )
-//    println("action " + action)
-//  }
-//
-//  "HttpsFilter" should "serve https requests" in {
-//    val request: Request[AnyContent] = FakeRequest("GET", "www.egraphs.com").withHeaders("x-forwarded-proto" -> "https")
-//    val mockConfig = mock[ConfigFileProxy]
-//    mockConfig.applicationHttpsOnly returns true
-//
-//    val httpsFilter = new HttpsFilter(mockConfig)
-//    val action = httpsFilter(Action { request => Ok } )
-//    println("action " + action)
-//  }
-//
-//  "HttpsFilter" should "serve http requests when httpsOnly is false" in {
-//    val request: Request[AnyContent] = FakeRequest("GET", "www.egraphs.com").withHeaders("x-forwarded-proto" -> "http")
-//    val mockConfig = mock[ConfigFileProxy]
-//    mockConfig.applicationHttpsOnly returns false
-//    
-//    val httpsFilter = new HttpsFilter(mockConfig)
-//    val action = httpsFilter(Action { request => Ok } )
-//    println("action " + action)
-//  }
+  "HttpsFilter" should "redirect insecure http requests to https when httpsOnly is true" in {
+    val result = resultFromTestRequest(protocol="http", httpsOnly=true)
+    
+    status(result) should be (SEE_OTHER)
+    redirectLocation(of=result) should be (Some("https://" + urlToTest))
+  }
+ 
+  it should "serve https requests" in {
+   val result = resultFromTestRequest(protocol="https", httpsOnly=true)
+ 
+   status(result) should be (OK)
+  }
+ 
+  it should "serve http requests when httpsOnly is false" in {
+    val result = resultFromTestRequest(protocol="http", httpsOnly=false)   
+ 
+    status(result) should be (OK)
+  }
+ 
+  private val urlToTest = "www.egraphs.com"
+ 
+  private def resultFromTestRequest(protocol: String, httpsOnly: Boolean): Result = {
+    val request = FakeRequest(GET, urlToTest).withHeaders("x-forwarded-proto" -> protocol)
+    val mockConfig = mock[ConfigFileProxy]
+    mockConfig.applicationHttpsOnly returns httpsOnly
+    
+    val httpsFilter = new HttpsFilter(mockConfig)
+    val action = httpsFilter(Action { request => Ok } )
+    
+    action(request)
+  }
 
 }
