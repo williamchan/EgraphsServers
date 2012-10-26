e97cf95d27abec39ba913d9b0e52a88e87a6d706
William Chan <will@egraphs.com>
2012-10-25 19:29:35 -0700

    SER-441: Extra quotations broke this controller. PostOrderConfigureEndpointTests would have caught this regression, and they will soon be reinstated.

--- apps/website/app/controllers/website/PostOrderConfigureEndpoint.scala
+++ apps/website/app/controllers/website/PostOrderConfigureEndpoint.scala
@@ -1,15 +1,15 @@
 package controllers.website
 
-import services.http.POSTControllerMethod
 import models._
 import enums.PrivacyStatus
-import play.api.mvc.Controller
-import services.http.SafePlayParams.Conversions._
-import sjson.json.Serializer
-import play.api.mvc._
+import play.api.data._
 import play.api.libs.json.Json.toJson
-import services.Utils
+import play.api.mvc.Controller
+import play.api.mvc._
+import play.api.data.Forms._
+import services.http.POSTControllerMethod
 import services.http.filters.HttpFilters
+import sjson.json.Serializer
 
 private[controllers] trait PostOrderConfigureEndpoint { this: Controller =>
   protected def postController: POSTControllerMethod
@@ -19,22 +19,16 @@ private[controllers] trait PostOrderConfigureEndpoint { this: Controller =>
   def postOrderPrivacy(orderId: Long) = postController() {
     httpFilters.requireCustomerLogin.inSession() { (customer, account) =>
       Action { implicit request =>
-        val params = request.queryString
-        val privacyStatusOption = Utils.getOptionFirstInSeq(params.get("privacyStatus"))
-
-        println("orderId " + orderId)
-        println("privacyStatusOption" + privacyStatusOption)
+        val privacyStatusString = Form("privacyStatus" -> text).bindFromRequest.fold(formWithErrors => "", validForm => validForm)
         val newPrivacyStatus = for (
-          privacyStatusString <- privacyStatusOption;
           privacyStatus <- PrivacyStatus(privacyStatusString);
           order <- orderStore.findById(orderId.toLong);
           if order.recipient.id == customer.id
         ) yield {
           order.withPrivacyStatus(privacyStatus).save().privacyStatus
         }
-  
         newPrivacyStatus match {
-          case Some(privacy) => Ok(toJson(Map("privacyStatus" -> "privacy.name")))
+          case Some(privacy) => Ok(toJson(Map("privacyStatus" -> privacy.name)))
           case None => Ok(toJson(Map("error" -> true))) //TODO: PLAY20 should this be a BadRequest?
         }
       }
