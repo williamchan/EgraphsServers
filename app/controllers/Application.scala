8fcf965649952053d01796091ee41fbcf9507b56
Stephanie Smallman <stephanie.gene@gmail.com>
2012-11-05 15:15:21 -0800

    Added Sheriff admin functionality for changing SNS alarm phone number

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,21 +6,25 @@ import play.api.libs.json.Json
 import play.api.mvc.Action
 import play.api.mvc.Controller
 import monitoring.cache.CacheMonitor
+import monitoring.alarms.AlarmUpdater
+import play.api.data.Form
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
 
   def index = Action { request =>
     Ok(views.html.index("Monitoring"))
   }
-  
+
   def websiteAvailability = Action {
-    Ok(views.html.website("WebsiteAvailability"))    
+    Ok(views.html.website("WebsiteAvailability"))
   }
 
   def dbAvailability = Action {
     Ok(views.html.db("DBAvailability"))
   }
-  
+
   def cacheAvailability = Action {
     Ok(views.html.cache("CacheAvailability"))
   }
@@ -44,5 +48,18 @@ object Application extends Controller {
     val jsonIterable = cacheMetrics.map(metric => metric.toJson)
     val responseJson = Json.toJson(Map("metrics" -> jsonIterable))
     Ok(responseJson)
+  }
+
+  /** The controllers below are for the Sheriff admin page */
+  
+  def alarms = Action {
+    Ok(views.html.alarms("SNSAlarms"))
+  }
+
+  def phoneUpdate = Action { request =>
+    def phone = request.body.asFormUrlEncoded.get("phone")(0)
+    val alarmUpdater = new AlarmUpdater
+    alarmUpdater.updateAlarm(phone)
+    Ok("The phone number associated with this alarm has been changed to: " + phone)
   }
 }
