cec8d7de7dd6651af391d84eea120e5df09cb2a5
Kevin Kyyro <kevinkyyro@gmail.com>
2012-11-09 12:17:51 -0800

    turning point; needs route enforcement, requires refactor starting now

--- modules/toybox/src/main/scala/egraphs/toybox/default/TBCookieChecker.scala
+++ modules/toybox/src/main/scala/egraphs/toybox/default/TBCookieChecker.scala
@@ -4,30 +4,35 @@ import play.api.mvc.Cookie
 import play.api.mvc.RequestHeader
 import egraphs.toybox.ToyBoxCookieChecker
 
+/* 
+ * TODO: read maxCookieAge from config or somethiiiing like that
+ */
+
 class TBCookieChecker(cookieName: String) extends ToyBoxCookieChecker {
-    val maxCookieAge = 20*60      // 20 minutes
+  // require non-null, non-empty cookie name
+  require(cookieName != null && cookieName.length > 0)
+  val maxCookieAge = 20*60      // 20 minutes
 
-    /**
-     * Generates the desired cookie. Default implementation only cares about the 
-     * cookies name and age, but
-     */
-    def generateCookie(request: RequestHeader): Option[Cookie] = {
-        // should the cookie contain any user information?
-        // seems it only needs an age and AuthenticityToken takes care of the rest
-        val cookieVal = "what should i be?"
-        Option(mkCookie(cookieName, cookieVal, maxCookieAge))
-    }
+  /**
+   * Generates the desired cookie. Default implementation only cares about the 
+   * cookies name and age, but
+   */
+  def generate(request: RequestHeader): Option[Cookie] = {
+    // should the cookie contain any user information?
+    // seems it only needs an age and AuthenticityToken takes care of the rest
+    val cookieVal = "you could make me a token if you wanted!"
+    Option(mkCookie(cookieName, cookieVal, maxCookieAge))
+  }
 
+  /**
+   * Checks if cookie is valid (exists). Default implementation just checks that it's 
+   * not expired, but more elaborate use cases include storing tokens in the cookie 
+   * and managing validity of token in some way, such as based on number of active users.
+   */
+  def validate(request: RequestHeader): Boolean = {
+    request != null && request.cookies.get(cookieName) != None
+  }
 
-    /**
-     * Checks if cookie is valid (exists). Default implementation just checks that it's 
-     * not expired, but more elaborate use cases include storing tokens in the cookie 
-     * and managing validity of token in some way, such as based on number of active users.
-     */
-    def validateCookie(request: RequestHeader): Boolean = {
-        request.cookies.get(cookieName).getOrElse(null) != null
-    }
-
-    private def mkCookie(name: String = "", value: String = "", maxAge: Int) = 
-        new Cookie(name, value, maxAge, "/", Option(null), false, false)
+  private def mkCookie(name: String = "", value: String = "", maxAge: Int) = 
+    new Cookie(name, value, maxAge, "/", Option(null), false, false)
 }
