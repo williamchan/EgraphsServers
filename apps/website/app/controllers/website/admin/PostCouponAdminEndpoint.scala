7adcb482f1dd36619efb5cdaa062da7a9ad1206d
Erem Boto <ehboto@gmail.com>
2012-11-15 18:58:03 -0800

    SER-264. Refactors coupon form code

--- apps/website/app/controllers/website/admin/PostCouponAdminEndpoint.scala
+++ apps/website/app/controllers/website/admin/PostCouponAdminEndpoint.scala
@@ -12,7 +12,7 @@ import services.http.POSTControllerMethod
 import services.http.filters.HttpFilters
 import services.http.SafePlayParams.Conversions._
 import services.logging.Logging
-import play.api.mvc.Action
+import play.api.mvc.{Action, Call, Result}
 import play.api.data._
 import play.api.data.Forms._
 import play.api.data.validation.Constraints._
@@ -22,6 +22,8 @@ import play.api.data.validation.Invalid
 
 trait PostCouponAdminEndpoint extends Logging {
   this: Controller =>
+
+  import PostCouponForm.fields
 
   protected def postController: POSTControllerMethod
   protected def httpFilters: HttpFilters
@@ -33,38 +35,20 @@ trait PostCouponAdminEndpoint extends Logging {
   def postCreateCouponAdmin = postController() {
     httpFilters.requireAdministratorLogin.inSession() { case (admin, adminAccount) =>
       Action { implicit request =>
-        val form = Form(mapping(
-                "name" -> text,
-                "code" -> nonEmptyText,
-                "startDate" -> nonEmptyText.verifying(isValidDateAndTime),
-                "endDate" -> nonEmptyText.verifying(isValidDateAndTime),
-                "discountAmount" -> number,
-                "couponTypeString" -> nonEmptyText.verifying(isCouponType),
-                "discountTypeString" -> nonEmptyText.verifying(isDiscountType),
-                "usageTypeString" -> nonEmptyText.verifying(isUsageType),
-                "restrictions" -> nonEmptyText
-        )(PostCouponForm.apply)(PostCouponForm.unapply)
-          verifying(codeIsUnique())
+        
+        Form(fields.verifying(codeIsUnique())).bindFromRequest.fold(
+          formWithErrors => {
+            val data = formWithErrors.data
+            val errors = for (error <- formWithErrors.errors) yield { error.key + ": " + error.message }
+            val url = controllers.routes.WebsiteControllers.getCreateCouponAdmin.url
+            Redirect(url).flashing(("errors" -> errors.mkString(", ")))
+          }, 
+          couponForm => {
+            val couponId = couponForm.configureCoupon().save().id
+
+            Redirect(controllers.routes.WebsiteControllers.getCouponAdmin(couponId).url)
+          }
         )
-          
-        form.bindFromRequest.fold(formWithErrors => {
-          val data = formWithErrors.data
-          val errors = for (error <- formWithErrors.errors) yield { error.key + ": " + error.message }
-          val url = controllers.routes.WebsiteControllers.getCreateCouponAdmin.url
-          Redirect(url).flashing(("errors" -> errors.mkString(", ")))
-	    }, validForm => {
-	      val coupon = Coupon(
-	          name = validForm.name,
-	          code = validForm.code,
-	          startDate = validForm.startDateTimestamp,
-	          endDate = validForm.endDateTimestamp,
-	          discountAmount = validForm.discountAmount,
-	          restrictions = validForm.restrictions
-	      ).withCouponType(validForm.couponType).withDiscountType(validForm.discountType).withUsageType(validForm.usageType)
-	      .save()
-	      
-          Redirect(controllers.routes.WebsiteControllers.getCouponAdmin(coupon.id).url)
-        })
       }
     }
   }
@@ -75,38 +59,19 @@ trait PostCouponAdminEndpoint extends Logging {
         couponStore.findById(couponId) match {
           case None => NotFound("No coupon with that id")
           case Some(coupon) => {
-            val form = Form(mapping(
-                "name" -> text,
-                "code" -> nonEmptyText,
-                "startDate" -> nonEmptyText.verifying(isValidDateAndTime),
-                "endDate" -> nonEmptyText.verifying(isValidDateAndTime),
-                "discountAmount" -> number,
-                "couponTypeString" -> nonEmptyText.verifying(isCouponType),
-                "discountTypeString" -> nonEmptyText.verifying(isDiscountType),
-                "usageTypeString" -> nonEmptyText.verifying(isUsageType),
-                "restrictions" -> nonEmptyText
-            )(PostCouponForm.apply)(PostCouponForm.unapply)
-              verifying(codeIsUnique(Some(couponId)))
+            Form(fields.verifying(codeIsUnique(Some(couponId)))).bindFromRequest.fold(
+              formWithErrors => {
+                val data = formWithErrors.data
+                val errors = for (error <- formWithErrors.errors) yield { error.key + ": " + error.message }
+                val url = controllers.routes.WebsiteControllers.getCouponAdmin(couponId).url
+                Redirect(url).flashing(("errors" -> errors.mkString(", ")))
+              }, 
+              couponForm => {
+                couponForm.configureCoupon(coupon).save
+  		      
+                Redirect(controllers.routes.WebsiteControllers.getCouponAdmin(couponId).url)
+              }
             )
-            
-            form.bindFromRequest.fold(formWithErrors => {
-              val data = formWithErrors.data
-	          val errors = for (error <- formWithErrors.errors) yield { error.key + ": " + error.message }
-              val url = controllers.routes.WebsiteControllers.getCouponAdmin(couponId).url
-              Redirect(url).flashing(("errors" -> errors.mkString(", ")))
-	        }, validForm => {
-		      coupon.copy(
-		          name = validForm.name,
-		          code = validForm.code,
-		          startDate = validForm.startDateTimestamp,
-		          endDate = validForm.endDateTimestamp,
-		          discountAmount = validForm.discountAmount,
-		          restrictions = validForm.restrictions
-		      ).withCouponType(validForm.couponType).withDiscountType(validForm.discountType).withUsageType(validForm.usageType)
-		      .save()
-		      
-              Redirect(controllers.routes.WebsiteControllers.getCouponAdmin(couponId).url)
-            })
           }
       	}
       }
@@ -129,8 +94,45 @@ trait PostCouponAdminEndpoint extends Logging {
     lazy val usageType = CouponUsageType(usageTypeString).get
     lazy val startDateTimestamp = new Timestamp(dateFormat.parse(startDate).getTime - localOffsetFromUtc)
     lazy val endDateTimestamp = new Timestamp(dateFormat.parse(endDate).getTime - localOffsetFromUtc)
+
+    /** 
+     * Configures a provided coupon or, by default, a new Coupon with the values
+     * supplied in the submitted form.
+     */
+    def configureCoupon(coupon: Coupon = Coupon()): Coupon  = {
+      coupon.copy(
+        name = name,
+        code = code,
+        startDate = startDateTimestamp,
+        endDate = endDateTimestamp,
+        discountAmount = discountAmount,
+        restrictions = restrictions
+      ).withCouponType(couponType)
+       .withDiscountType(discountType)
+       .withUsageType(usageType)
+    }
   }
-  
+
+  private object PostCouponForm {
+    /** The Play2 Form API field mappings for this form */
+    val fields = {
+      mapping(
+        "name" -> text,
+        "code" -> nonEmptyText,
+        "startDate" -> nonEmptyText.verifying(isValidDateAndTime),
+        "endDate" -> nonEmptyText.verifying(isValidDateAndTime),
+        "discountAmount" -> number,
+        "couponTypeString" -> nonEmptyText.verifying(isCouponType),
+        "discountTypeString" -> nonEmptyText.verifying(isDiscountType),
+        "usageTypeString" -> nonEmptyText.verifying(isUsageType),
+        "restrictions" -> nonEmptyText
+      )(PostCouponForm.apply)(PostCouponForm.unapply)
+    }
+  }
+
+  //
+  // Private members
+  //
   private def codeIsUnique(couponId: Option[Long] = None): Constraint[PostCouponForm] = {
     Constraint { form: PostCouponForm =>
       (couponStore.findValid(form.code), couponId) match {
