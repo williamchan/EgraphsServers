c956df0513d9d2183ad144aff1687ee308b4ba97
Erem Boto <ehboto@gmail.com>
2012-12-05 17:07:56 -0800

    SER-499. Inefficient-as-fuck implementation of Checkout.lineItems

--- apps/website/app/models/checkout/Checkout.scala
+++ apps/website/app/models/checkout/Checkout.scala
@@ -1,10 +1,52 @@
 package models.checkout
 
-import org.joda.money.Money
 
-trait Checkout {
-  def lineItems: Seq[LineItem[_]]
+case class Checkout(lineItemTypes: IndexedSeq[LineItemType[_]]) {
 
-  def subtotal: Money
-  def total: Money
+  def add(lineItemType: LineItemType[_]): Checkout = {
+    this.copy(lineItemTypes = lineItemTypes :+ lineItemType)
+  }
+
+  lazy val lineItems: IndexedSeq[LineItem[_]] = {
+    case class ResolutionPass(items: IndexedSeq[LineItem[_]], unresolved: IndexedSeq[LineItemType[_]]) {
+      def isComplete: Boolean = unresolved.isEmpty
+    }
+
+    def executePass(passToResolve: ResolutionPass): IndexedSeq[LineItem[_]] = {
+      if (passToResolve.isComplete) {
+        passToResolve.items
+      } else {
+        val resolvedPass = passToResolve.unresolved.foldLeft(passToResolve) { (oldPass, nextItemType) =>
+          val itemTypesSansCurrent = oldPass.unresolved.filter(_ != nextItemType)
+
+          nextItemType.lineItems(oldPass.items, itemTypesSansCurrent) match {
+            case Some(newLineItems) => oldPass.copy(newLineItems, itemTypesSansCurrent)
+            case None => oldPass
+          }
+        }
+
+        // Check to make sure we're not in a circular dependency loop
+        assert(
+          resolvedPass.unresolved.length != passToResolve.unresolved.length,
+          "Attempt to resolve LineItemTypes to LineItems failed to resolve even one: " +
+            passToResolve.items +
+            "\n\n" +
+            passToResolve.unresolved
+        )
+
+        executePass(resolvedPass)
+      }
+    }
+
+    val initialPass = ResolutionPass(IndexedSeq(), lineItemTypes)
+
+    executePass(initialPass)
+  }
+
+  lazy val flattenedLineItems: IndexedSeq[LineItem[_]] = {
+    for (lineItem <- lineItems; flattened <- lineItem.flatten) yield flattened
+  }
+
+  // def subtotal: Money
+  // def total: Money
 }
