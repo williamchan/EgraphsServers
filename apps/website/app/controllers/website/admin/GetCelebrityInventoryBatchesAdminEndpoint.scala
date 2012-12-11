f533239342989a02d63e56a99c36800287fc2eeb
myyk <myyk@egraphs.com>
2012-12-11 14:52:58 -0800

    SER-612 Show remaining inventory on inventory batch summary admin page for help in correct manual corrections to quantities.

--- apps/website/app/controllers/website/admin/GetCelebrityInventoryBatchesAdminEndpoint.scala
+++ apps/website/app/controllers/website/admin/GetCelebrityInventoryBatchesAdminEndpoint.scala
@@ -18,16 +18,17 @@ private[controllers] trait GetCelebrityInventoryBatchesAdminEndpoint extends Imp
   protected def httpFilters: HttpFilters
   protected def inventoryBatchStore: InventoryBatchStore
   protected def inventoryBatchQueryFilters: InventoryBatchQueryFilters
+  protected def celebrityStore: CelebrityStore
 
   def getCelebrityInventoryBatchesAdmin(celebrityId: Long) = controllerMethod.withForm() { implicit authToken => 
     httpFilters.requireAdministratorLogin.inSession() { case (admin, adminAccount) =>
       httpFilters.requireCelebrityId(celebrityId) { (celebrity) =>
         Action { implicit request =>
-          
+
           // get query parameters
           val page: Int = Form("page" -> number).bindFromRequest.fold(formWithErrors => 1, validForm => validForm)
           val filter: String = Form("filter" -> text).bindFromRequest.fold(formWithErrors => "all", validForm => validForm)
-          
+
           val query = filter match {
             case "activeOnly" => inventoryBatchStore.findByCelebrity(celebrity.id, inventoryBatchQueryFilters.activeOnly)
             case "all" => inventoryBatchStore.findByCelebrity(celebrity.id)
@@ -35,7 +36,16 @@ private[controllers] trait GetCelebrityInventoryBatchesAdminEndpoint extends Imp
           }
           val pagedQuery: (Iterable[InventoryBatch], Int, Option[Int]) = services.Utils.pagedQuery(select = query, page = page)
           implicit val paginationInfo = PaginationInfoFactory.create(pagedQuery = pagedQuery, baseUrl = GetCelebrityInventoryBatchesAdminEndpoint.url(celebrity.id), filter = Option(filter))
-          Ok(views.html.Application.admin.admin_inventorybatches(inventoryBatches = pagedQuery._1, celebrity = celebrity))
+
+          // also display information from catalog stars query for manual discrepancy resolution
+          val stars = celebrityStore.getCatalogStars.filter(star => star.id == celebrity.id)
+          val inventoryRemaining = if(stars.size == 1) {
+            stars.head.inventoryRemaining
+          } else {
+            0
+          }
+
+          Ok(views.html.Application.admin.admin_inventorybatches(inventoryBatches = pagedQuery._1, celebrity = celebrity, inventoryRemaining = inventoryRemaining))
         }
       }
     }
