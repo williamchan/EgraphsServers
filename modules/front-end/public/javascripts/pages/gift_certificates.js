42ac4128d1482f91c4f80fc09619165dfcf01c0b
Erem Boto <ehboto@gmail.com>
2012-12-11 15:01:44 -0800

    SER-600. Makes review order modal.

--- modules/front-end/public/javascripts/pages/gift_certificates.js
+++ modules/front-end/public/javascripts/pages/gift_certificates.js
@@ -1,10 +1,11 @@
 /* Scripting for the gift certificates checkout page */
-define(["services/forms", "services/payment", "Egraphs", "libs/chosen/chosen.jquery.min"],
+define(["services/forms", "services/payment", "Egraphs", "libs/chosen/chosen.jquery.min", "bootstrap/bootstrap-modal"],
 function(forms, payment, Egraphs) {
   return { 
     go: function() { 
       console.log("Herp derp");
       $(".chsn-select").chosen({no_results_text: "No results matched"});
+      // $("#review").modal("toggle")
     }
   };
 });
