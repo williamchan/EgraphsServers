2381d0183c5fcf95d499606adb9b4125e9aa1c0e
Erem Boto <ehboto@gmail.com>
2012-12-13 12:17:45 -0800

    SER-600. Final complete layout.

--- modules/front-end/public/javascripts/pages/gift_certificates.js
+++ modules/front-end/public/javascripts/pages/gift_certificates.js
@@ -4,13 +4,11 @@ define([
   "services/payment",
   "Egraphs",
   "libs/chosen/chosen.jquery.min",
-  "bootstrap/bootstrap-modal",
   "bootstrap-modal/bootstrap-modalmanager",
   "bootstrap-modal/bootstrap-modal"],
 function(forms, payment, Egraphs) {
   return {
     go: function() {
-      console.log("Herp derp");
       $(".chsn-select").chosen({no_results_text: "No results matched"});
       $("#review-button").click(function() {
         $("#review").modal("toggle");
