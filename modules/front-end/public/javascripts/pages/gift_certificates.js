9f7cb28b2654c20a0ce3b78f6bc967eb506eb170
Erem Boto <ehboto@gmail.com>
2012-12-12 17:08:26 -0800

    SER-600. Initial design of the Review page.

--- modules/front-end/public/javascripts/pages/gift_certificates.js
+++ modules/front-end/public/javascripts/pages/gift_certificates.js
@@ -1,11 +1,18 @@
 /* Scripting for the gift certificates checkout page */
-define(["services/forms", "services/payment", "Egraphs", "libs/chosen/chosen.jquery.min", "bootstrap/bootstrap-modal"],
+define([
+  "services/forms",
+  "services/payment",
+  "Egraphs",
+  "libs/chosen/chosen.jquery.min",
+  "bootstrap/bootstrap-modal",
+  "bootstrap-modal/bootstrap-modalmanager",
+  "bootstrap-modal/bootstrap-modal"],
 function(forms, payment, Egraphs) {
-  return { 
-    go: function() { 
+  return {
+    go: function() {
       console.log("Herp derp");
       $(".chsn-select").chosen({no_results_text: "No results matched"});
-      // $("#review").modal("toggle")
+      $("#review").modal("toggle")
     }
   };
 });
