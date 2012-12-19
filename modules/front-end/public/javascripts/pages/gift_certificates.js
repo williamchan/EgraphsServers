4d163c0551118c14b21697c5e88c314f5a44bb8e
Erem Boto <ehboto@gmail.com>
2012-12-19 11:23:01 -0800

    SER-630. Implements SET MY OWN AMOUNT dynamics and selection dynamics.

--- modules/front-end/public/javascripts/pages/gift_certificates.js
+++ modules/front-end/public/javascripts/pages/gift_certificates.js
@@ -1,4 +1,6 @@
 /* Scripting for the gift certificates checkout page */
+
+/*global angular */
 define([
   "services/forms",
   "services/payment",
@@ -6,8 +8,50 @@ define([
   "libs/chosen/chosen.jquery.min",
   "services/responsive-modal"],
 function(forms, payment, Egraphs) {
+  var GiftCertificateOption = function(data) {
+    this.price = data.price;
+    this.type = data.type;
+    this.isMostPopular = data.isMostPopular;
+    this.isConcrete = data.type === "concrete";
+    this.isCustom = data.type === "custom";
+    this.buttonSpanSize = this.isConcrete? "span2": "span4";
+
+    this.select(false);
+  };
+
+  GiftCertificateOption.prototype.select = function(select) {
+    this.selected = select;
+    this.buttonClass = select? "active": "";
+    this.amountFieldClass = select? "scrolled-out": "scrolled-in";
+  };
+
+  var certificateOptions = [];
+  angular.forEach(Egraphs.page.certificateOptions, function(option) {
+    certificateOptions.push(new GiftCertificateOption(option));
+  });
+
+  var GiftCertificatePurchaseController = function ($scope) {
+    $scope.certificateOptions = certificateOptions;
+
+    $scope.selectOption = function(certificateOption) {
+      // Toggle selected state
+      certificateOption.select(true);
+
+      if ($scope.selectedOption) $scope.selectedOption.select(false);
+
+      $scope.selectedOption = certificateOption;
+    };
+
+    
+    angular.forEach(certificateOptions, function(option) {
+      if (option.isMostPopular) $scope.selectOption(option);
+    });
+  };
+
   return {
     go: function() {
+      window.GiftCertificatePurchaseController = GiftCertificatePurchaseController;
+
       $(".chsn-select").chosen({no_results_text: "No results matched"});
 
       $("#review-button").click(function() {
@@ -16,6 +60,10 @@ function(forms, payment, Egraphs) {
 
       $(".modify-order").click(function() {
         $("#review").responsivemodal("toggle");
+      });
+
+      angular.element(document).ready(function() {
+        angular.bootstrap(document);
       });
     }
   };
