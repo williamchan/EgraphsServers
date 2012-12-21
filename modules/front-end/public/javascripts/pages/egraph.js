447b8367834e8aaf815a0dd0927f22db75102d61
Siegfried Bilstein <siegfried@egraphs.com>
2012-12-20 17:12:50 -0800

    SER-536 Hello world with an overlay!

--- modules/front-end/public/javascripts/pages/egraph.js
+++ modules/front-end/public/javascripts/pages/egraph.js
@@ -1,8 +1,18 @@
 /* Scripting for the egraph page */
-define(["Egraphs"], function (Egraphs) {
+define(["Egraphs", "libs/guiders"], function (Egraphs, guiders) {
 	return {
     go: function () {
-      console.log("hello world!");
+      guiders.createGuider({
+        attachTo: ".sm2-360btn",
+        buttons: [{name: "Close", onclick: guiders.hideAll}],
+        classString: "egraph-guider",
+        description: "Hello World!",
+        id: "first",
+        overlay: true,
+        position: 6,
+        title: "You can also advance guiders from custom event handlers.",
+        width: 450
+      }).show();
     }
   };
 });
