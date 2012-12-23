4b881bb6ed7e9bbf387279bfe1897ae5bfc28f10
William Chan <will@egraphs.com>
2012-12-23 01:43:05 -0800

    SER-579: Delete most holiday copy and remove holiday alert from purchase flow.

--- modules/front-end/public/javascripts/pages/celebrity-storefront-review.js
+++ modules/front-end/public/javascripts/pages/celebrity-storefront-review.js
@@ -2,22 +2,12 @@
 define(["services/forms", "Egraphs", "bootstrap/bootstrap-modal"],
 function(forms, Egraphs) {
 
-  var alertedRegardingHolidayPrintDelivery = false;
-
   return {
     go: function() {
       forms.setIphoneCheckbox('#order-print', {
       	checkedLabel: 'YES', 
       	uncheckedLabel: 'NO'
       });
-
-      $('.iPhoneCheckContainer').click(function() {
-        if (!alertedRegardingHolidayPrintDelivery && $('#order-print')[0].checked === true) {
-          alertedRegardingHolidayPrintDelivery = true;
-          alert("Just making sure you know that this will not arrive by December 24. \n\nStill want the framed print? \n\n:-)");
-        }
-      });
-
 
       // Mixpanel events
       mixpanel.track_forms('.container form', 'Checkout clicked');
