b7723555c684c89506efece036a5520f66604dc2
Siegfried Bilstein <siegfried@egraphs.com>
2013-01-14 18:03:29 -0800

    HE-13 refactored homepage into tabular view.

---
+++ modules/front-end/public/javascripts/pages/landing-a.js
@@ -0,0 +1,32 @@
+/* Scripting for the landing page */
+define(["bootstrap/bootstrap-tooltip", "bootstrap/bootstrap-popover", "bootstrap/bootstrap-tab"],
+function () {
+  return {
+    go: function() {
+      $(document).ready(function() {
+        $(".soldout-tooltip").tooltip({placement:"top"});
+
+        // Mixpanel events
+        mixpanel.track('Home page viewed', {'query': window.location.search });
+        mixpanel.track_links('#get-started-button', 'Get Started clicked');
+        $(".celebrities figure>a").click(function() {
+          mixpanel.track("Celebrity clicked");
+        });
+        $(".celebrities h4>a").click(function() {
+          mixpanel.track("Celebrity clicked");
+        });
+
+        $(".switcher-tab").click(function(e){
+          var tab = $(this);
+          var parent = tab.parent();
+          parent.addClass("active");
+          parent.siblings().removeClass("active");
+
+          $(".tab").hide();
+          $(tab.attr("href")).fadeIn();
+          e.preventDefault();
+        });
+      });
+    }
+  };
+});
