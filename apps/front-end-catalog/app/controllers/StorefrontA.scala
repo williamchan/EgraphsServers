78b2c52dae90b3bd1f16e6369adcacfe9983b6d9
Erem Boto <ehboto@gmail.com>
2013-01-24 15:39:17 -0800

    CE-9. Checkout page layout and styles.

--- apps/front-end-catalog/app/controllers/StorefrontA.scala
+++ apps/front-end-catalog/app/controllers/StorefrontA.scala
@@ -7,4 +7,8 @@ object StorefrontA extends Controller with DefaultImplicitTemplateParameters {
   def personalize = Action {
     Ok(views.html.frontend.storefronts.a.personalize())
   }
+
+  def checkout = Action {
+    Ok(views.html.frontend.storefronts.a.checkout())
+  }
 }
