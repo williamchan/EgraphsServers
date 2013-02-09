8ed3cce418f9c1d131c22adb2ba4c1851684b119
Erem Boto <ehboto@gmail.com>
2013-02-08 16:22:33 -0800

    Fixes angular configuration issue for password reset page.

--- modules/front-end/public/javascripts/pages/account-password-reset.js
+++ modules/front-end/public/javascripts/pages/account-password-reset.js
@@ -1,21 +1,23 @@
 /* Scripting for the password reset page */
+/*global angular*/
 define(["Egraphs"], function (Egraphs) {
-  var Controller = function ($scope) {
-    $scope.master = angular.copy(Egraphs.page.user);
-
-    $scope.update = function (user) {
-      $scope.master= angular.copy(user);
-    };
-  };
   return {
-  /**
-   * Executes all the scripts for the password reset template
-   *
-   * @return nothing
-   */
-    go: function () {
-      window.Controller = Controller;
+    ngControllers: {
+      PasswordMatchController: ["$scope", function ($scope) {
+        $scope.master = angular.copy(Egraphs.page.user);
 
+        $scope.update = function (user) {
+          $scope.master= angular.copy(user);
+        };
+      }]
+    },
+
+   /**
+    * Executes all the scripts for the password reset template
+    *
+    * @return nothing
+    */
+    go: function () {
      $("#user_password_confirmation").change(function (e) {
        var password = $("#user_password");
        var password_confirmation = $("#user_password_confirmation");
@@ -26,7 +28,6 @@ define(["Egraphs"], function (Egraphs) {
          $("#password_message").removeClass("invisible");
        }
      });
-
     }
   };
 });
