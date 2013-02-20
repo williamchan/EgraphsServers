b264935937434fb80ce364498bbf712f09c07a44
William Chan <will@egraphs.com>
2013-02-20 14:55:25 -0800

    Damn, revert this line too to stop exceptions from inconsistencies between Address table and model.

--- apps/website/app/models/Address.scala
+++ apps/website/app/models/Address.scala
@@ -41,7 +41,7 @@ case class Address(
   def streetAddressString = {
     val address = (List(addressLine1) ++  addressLine2.toList) mkString (" ")
     val stateAndZip = "%s, %s %s".format(city, _state, postalCode)
-    val streetAddressAsList = name.toList ++ List(address, city, stateAndZip)
+    val streetAddressAsList = List(address, city, stateAndZip)
     streetAddressAsList.mkString("\n")
   }
 
