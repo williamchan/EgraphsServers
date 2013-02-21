b2e91fb21a2b29116d1cd0b325510bd105a6c8fc
William Chan <will@egraphs.com>
2013-02-21 15:09:14 -0800

    Add some Mixpanel events to new egraph page.

---
+++ modules/front-end/public/javascripts/pages/egraph.js
@@ -0,0 +1,14 @@
+/* Scripting for the egraph classic page */
+define(["Egraphs"], function (Egraphs) {
+  return {
+    go: function () {
+      // Mixpanel events
+      mixpanel.track('Egraph viewed', {'url': window.location.href});
+      $('.vjs-big-play-button').click(function() {
+        mixpanel.track("Egraph video played");
+      });
+      mixpanel.track_links('.pin-it-button', 'Pin It button clicked')
+      mixpanel.track_links('#classic-link', 'Classic egraph mode clicked')
+    }
+  };
+});
