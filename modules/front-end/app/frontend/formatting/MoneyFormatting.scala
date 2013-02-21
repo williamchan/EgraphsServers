13ed343cf02fd4797f9c0fc684cec993ac5ced0c
Myyk Seok <myyk.seok@gmail.com>
2013-02-21 11:48:17 -0800

    Fix some comments

--- modules/front-end/app/frontend/formatting/MoneyFormatting.scala
+++ modules/front-end/app/frontend/formatting/MoneyFormatting.scala
@@ -5,7 +5,7 @@ import java.math.RoundingMode
 import org.joda.money.{CurrencyUnit, Money}
 
 /**
- * Utilities for formatting [[org.joda.cash.Money]].
+ * Utilities for formatting [[org.joda.money.Money]].
  *
  * Most of the utilities are best used implicitly by importing Finance.TypeConversions._
  *
@@ -17,7 +17,7 @@ import org.joda.money.{CurrencyUnit, Money}
  *   val amount = BigDecimal(100.50)
  *
  *   val dollars = amount.toMoney()
- *   val euros = amount.toMoney(org.joda.cash.CurrencyUnit.EUR)
+ *   val euros = amount.toMoney(org.joda.money.CurrencyUnit.EUR)
  *
  *   // Format the cash
  *   val forDisplay = dollars.formatSimply // -> $100.50
