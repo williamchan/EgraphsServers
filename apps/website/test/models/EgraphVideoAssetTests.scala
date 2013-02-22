c2fb810fac077295fa152553e35b25fd88e4eb20
William Chan <will@egraphs.com>
2013-02-22 13:50:56 -0800

    Fix unit test for video encoding, which will generate slightly different files based on operating system.

--- apps/website/test/models/EgraphVideoAssetTests.scala
+++ apps/website/test/models/EgraphVideoAssetTests.scala
@@ -9,6 +9,10 @@ class EgraphVideoAssetTests extends EgraphsUnitTest with DBTransactionPerTest {
 
   "encodeVideo" should "return byte array of video-fied egraph" in new EgraphsTestApplication {
     val egraph = TestData.newSavedEgraphWithRealAudio()
-    egraph.getVideoAsset.encodeVideo().length should be(52209)
+    val egraphVideoLength = egraph.getVideoAsset.encodeVideo().length
+
+    // Xuggle calls native code libraries that are different for various operating systems.
+    // 52011 is expected on Mac, 52209 on Linux
+    List(52011, 52209) should contain(egraphVideoLength)
   }
 }
