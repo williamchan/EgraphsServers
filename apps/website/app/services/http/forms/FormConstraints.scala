8842b03e144b3198abc9414e34c3cda25c61d89c
Siegfried Bilstein <siegfried@egraphs.com>
2013-03-05 09:36:51 -0800

    SER-700 a more helpful error message and a TODO

--- apps/website/app/services/http/forms/FormConstraints.scala
+++ apps/website/app/services/http/forms/FormConstraints.scala
@@ -7,6 +7,9 @@ import play.api.data.validation.{ Valid, Invalid }
 import models.Password.{PasswordRequired, PasswordTooShort}
 
 class FormConstraints @Inject() (accountStore: AccountStore) {
+   // TODO: think about making a composable constraint class, so that we can cascade errors. An implementation similar to
+   // filters is an idea. See this discussion: https://github.com/Egraphs/eGraphsServers/pull/369/files#L2L13
+
   /**
    * This constraint takes an optional additional constraint in the form of a function. If and only if the function
    * returns true, then the email is invalid, so the constraint is an AND on the filters, limiting the scope of the
@@ -20,10 +23,11 @@ class FormConstraints @Inject() (accountStore: AccountStore) {
    * @param constraint
    * @return
    */
-  def isValidNewEmail(constraint: Account => Boolean = { account => true} ): Constraint[String] = {
+
+  def isValidNewEmail(constraint: Account => Boolean = { account => true}): Constraint[String] = {
     Constraint { email: String =>
       accountStore.findByEmail(email) match {
-        case Some(preexistingAccount) if constraint(preexistingAccount) => Invalid("Account with e-mail address already exists")
+        case Some(preexistingAccount) if constraint(preexistingAccount) => Invalid("Account with e-mail address (" + email + ") already exists")
         case _ => Valid
       }
     }
