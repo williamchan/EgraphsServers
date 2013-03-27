7c9bc2b684fa54cf6a295b4103a54ebee5e9a240
Kevin Kyyro <kevinkyyro@gmail.com>
2013-03-27 02:19:47 -0700

    HE-64 added social event tracking to analytics.js and instrumented egraph page with it. needs to be tested.

--- modules/front-end/public/javascripts/pages/egraph.js
+++ modules/front-end/public/javascripts/pages/egraph.js
@@ -1,15 +1,64 @@
 /* Scripting for the egraph classic page */
-define(["Egraphs"], function (Egraphs) {
+define(["Egraphs", "services/analytics"], function (Egraphs, analytics) {
+  var events = analytics.eventCategory("Egraph");
+  var social = events.social;
+  var $playButton = function() { return $('.vjs-big-play-button'); };
+
+  var mouseOnPlayButton = function() { return $playButton().is(':hover'); };
+
   return {
     go: function () {
       // Mixpanel events
       mixpanel.track('Egraph viewed', {'url': window.location.href});
-      $('.vjs-big-play-button').click(function() {
-        mixpanel.track("Egraph video played");
-      });
       mixpanel.track_links('#promo-shop-link', 'Shop button clicked from egraph');
       mixpanel.track_links('.pin-it-button', 'Pin It button clicked');
       mixpanel.track_links('#classic-link', 'Classic egraph mode clicked');
+      $playButton.click(function() {
+        mixpanel.track("Egraph video played");
+      });
+
+      //
+      // Google Analytics events
+      //
+      $('#egraph-player').click(function(e){
+        if (!mouseOnPlayButton()) {
+          events.track("Playback", "Egraph player clicked");
+        }
+      });
+
+      $playButton().click(function(e) {
+        events.track("Playback", "Big play button clicked");
+      });
+
+      $('#promo-shop-link').click(function(e){
+        events.track('Promo', 'Get Your Own clicked');
+      });
+
+      $('#classic-link').click(function(e){
+        events.track('Classic', "Visited from new egraph");
+      });
+
+      //
+      // Facebook Events
+      // https://developers.google.com/analytics/devguides/collection/gajs/gaTrackingSocial#facebook
+      FB.Event.subscribe('edge.create', social.fb.like.track);
+      FB.Event.subscribe('edge.remove', social.fb.unlike.track);
+      FB.Event.subscribe('message.send', social.fb.share.track);
+
+      //
+      // Twitter Events
+      // https://developers.google.com/analytics/devguides/collection/gajs/gaTrackingSocial#twitter
+      var tweetTracker = social.twitter.tweet.track;
+      var bindTweetTracker = function(twttr) { twttr.events.bind('tweet', tweetTracker) };
+      twttr.ready(bindTweetTracker);
+
+      //
+      // Pinterest events
+      //
+      $('.pin-it-button').click(function(e){
+        social.pinterest.pin.track();
+      });
+
     }
   };
 });
--- modules/front-end/public/javascripts/services/analytics.js
+++ modules/front-end/public/javascripts/services/analytics.js
@@ -14,6 +14,12 @@ function(window, logging, requireModule) {
     google.push(gaqArguments);
   };
 
+  var googleTrackSocialEvent = function(args) {
+    var gaqArguments = ["_trackSocial"].concat(args);
+    log("Google Social Event: " + args);
+    google.push(gaqArguments);
+  };
+
   var googleTrackTiming = function(args) {
     var gaqArguments = ["_trackTiming"].concat(args);
     log("Google Timing Event: " + args);
@@ -32,9 +38,56 @@ function(window, logging, requireModule) {
 
   return {
     eventCategory: function(categoryName) {
+      var trackInCategory = function(actionAndLabel) {
+        googleTrackEvent([categoryName].concat(actionAndLabel));
+      };
+
+      var socialEvent = function(network, action) {
+        var networkAndAction = [network, action];
+        return {
+          track: function(targetUrl) {
+            // filter falsy values (e.g. undefined if called with no args)
+            var targetArgArray = [targetUrl].filter(Boolean); 
+            
+            // not sure whether social events are as useful as normal events, so let's track both
+            trackInCategory(networkAndAction);
+            googleTrackSocialEvent(networkAndAction.concat(targetUrlArray));
+          }
+        }
+      };
+
       var self = {
         track: function(actionAndLabel) {
-          googleTrackEvent([categoryName].concat(actionAndLabel));
+          trackInCategory(actionAndLabel);
+        },
+
+        // tracking for social events (e.g. widget interactions); organized by network, then action,
+        // and then finally a track method
+        social: {
+          fb: {
+            like:   socialEvent('Facebook', 'Like'),
+            unlike: socialEvent('Facebook', 'Unlike'),
+            share:  socialEvent('Facebook', 'Share')
+          },
+
+          twitter: {
+            tweet: {
+              // https://developers.google.com/analytics/devguides/collection/gajs/gaTrackingSocial#twitter
+              track: function(intent_event) {
+                if (intent_event) {
+                  var opt_pagePath;
+                  if (intent_event.target && intent_event.target.nodeName == 'IFRAME') {
+                    opt_target = extractParamFromUri(intent_event.target.src, 'url');
+                  }
+                  socialEvent('Twitter', 'Tweet').track(opt_pagePath);
+                }
+              }
+            }
+          },
+
+          pinterest: {
+            pin: socialEvent(['Pinterest', 'Pinned'])
+          }
         },
 
         startEvent: function(actionLabelAndSampleRate) {
